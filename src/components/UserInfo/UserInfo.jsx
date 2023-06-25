09cda28a885a69f531b11bc741b10825411b316a
Pavlo Kuraksin <kuraksinpavel@gmail.com>
2023-06-25 23:12:55 +0300

    add task solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,4 +1,10 @@
 // Add the required props
-export const UserInfo = () => (
-  <>UserInfo markup</>
+export const UserInfo = ({ user }) => (
+  <>
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      <p>
+        {user.name}
+      </p>
+    </a>
+  </>
 );
