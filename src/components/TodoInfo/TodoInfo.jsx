36c178fac42f41648d83160de38a40972ae9bc39
maxyma-sanx <maxymasan.dev@gmail.com>
2023-06-28 17:42:53 +0300

    add classList

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,15 +1,14 @@
+import classNames from 'classnames';
 import { UserInfo } from '../UserInfo';
 import './TodoInfo.scss';
 
-export const TodoInfo = ({ todo: { user, title, completed } }) => {
-  const isCompleted = completed ? 'TodoInfo--completed' : '';
-
-  return (
-    <>
-      <article className={`TodoInfo ${isCompleted}`}>
-        <h2 className="TodoInfo__title">{title}</h2>
-        {user && <UserInfo user={user} />}
-      </article>
-    </>
-  );
-};
+export const TodoInfo = ({ todo: { user, title, completed } }) => (
+  <>
+    <article className={
+      classNames('TodoInfo', { 'TodoInfo--completed': completed })}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+      {user && <UserInfo user={user} />}
+    </article>
+  </>
+);
