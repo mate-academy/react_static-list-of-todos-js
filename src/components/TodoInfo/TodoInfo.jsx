bb3076e9065fb9c91679c4b7f1eabbd580324ea9
Aleks <offsset@gmail.com>
2023-06-28 20:58:36 +0300

    Solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -2,7 +2,7 @@ import { UserInfo } from '../UserInfo/UserInfo';
 
 export const TodoInfo = ({ title, user }) => (
   <article className="TodoInfo TodoInfo--completed">
-    <h2 className="TodoInfo__title">{title}</h2>
+    <h2 className="TodoInfo__title TodoInfo--completed">{title}</h2>
     <UserInfo
       user={user}
     />
