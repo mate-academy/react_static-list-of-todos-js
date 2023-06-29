7c4e003ffcfa63e0c33167d232ce12d1b0b04cc1
artkas03 <zhonayer2017@gmail.com>
2023-06-29 16:24:18 +0300

    finished task

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,18 @@
-// Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import { UserInfo } from '../UserInfo';
+
+export const TodoInfo = ({ todo }) => {
+  const {
+    completed,
+    title,
+    user,
+  } = todo;
+
+  const classes = `TodoInfo ${completed ? 'TodoInfo--completed' : ''}`;
+
+  return (
+    <article className={classes}>
+      <h2 className="TodoInfo__title">{title}</h2>
+      <UserInfo user={user} />
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,4 +1,14 @@
 // Add the required props
-export const UserInfo = () => (
-  <>UserInfo markup</>
-);
+export const UserInfo = ({ user }) => {
+  if (user === undefined) {
+    return '';
+  }
+
+  const userMailLink = `mailto:${user.email}`;
+
+  return (
+    <a className="UserInfo" href={userMailLink}>
+      {user.name}
+    </a>
+  );
+};
