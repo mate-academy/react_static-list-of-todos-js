a4a5eaf14402403e3a46ab125aee684d5e68994b
Olha Sheliakina <134736240+OlhaSheliakina@users.noreply.github.com>
2023-06-29 23:18:08 +0300

    Update TodoList.jsx

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -3,7 +3,10 @@ import { TodoInfo } from '../TodoInfo';
 export const TodoList = ({ todos }) => (
   <section className="TodoList">
     {todos.map(todo => (
-      <TodoInfo todo={todo} />
+       <TodoInfo
+        todo={todo}
+        key={todo.id}
+      />
     ))}
   </section>
 );
