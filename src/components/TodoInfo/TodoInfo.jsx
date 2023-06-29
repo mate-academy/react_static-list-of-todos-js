b61255acfae83d912ad40842caa74ad5bd6af580
Nataliia Hrushanyk <nataligrushanyk@gmail.com>
2023-06-29 15:16:40 +0300

    eact_static-list-of-todos-js solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,14 @@
-// Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import { UserInfo } from '../UserInfo';
+import './TodoInfo.scss';
+
+export const TodoInfo = ({ todo: { completed, title, user } }) => {
+  const completedClass = completed ? ' TodoInfo--completed' : '';
+
+  return (
+    <article className={`TodoInfo${completedClass}`}>
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      <UserInfo user={user} />
+    </article>
+  );
+};
