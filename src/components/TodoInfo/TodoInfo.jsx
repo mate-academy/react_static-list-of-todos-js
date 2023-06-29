daf81f41311f63a95816de5cf520b5ab620c2272
Ihor Oksentiuk <winbka.lem@gmail.com>
2023-06-29 19:46:46 +0300

    Created info component

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,14 @@
-// Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import { UserInfo } from '../UserInfo';
+import './TodoInfo.scss';
+
+export const TodoInfo = ({ todo }) => {
+  const isCompleted = todo.completed ? 'TodoInfo--completed' : '';
+
+  return (
+    <article className={`TodoInfo ${isCompleted}`}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      <UserInfo user={todo.user} />
+    </article>
+  );
+};
