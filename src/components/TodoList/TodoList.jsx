bb6e9218ea4de148c8daa5ff50df7e92377c2b8d
artkas03 <zhonayer2017@gmail.com>
2023-06-29 16:28:32 +0300

    small style corrections

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,9 +1,9 @@
 import { TodoInfo } from '../TodoInfo';
 
 export const TodoList = ({ todos }) => (
-  <>
+  <div className="TodoList">
     {todos.map(todo => (
       <TodoInfo todo={todo} key={todo.id} />
     ))}
-  </>
+  </div>
 );
