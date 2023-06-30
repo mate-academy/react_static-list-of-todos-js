7b8ca5a5a4c496876eca25dd4705b5ebb03ba6ac
maxyma-sanx <maxymasan.dev@gmail.com>
2023-06-30 10:11:59 +0300

    solution fix

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -3,12 +3,10 @@ import { UserInfo } from '../UserInfo';
 import './TodoInfo.scss';
 
 export const TodoInfo = ({ todo: { user, title, completed } }) => (
-  <>
-    <article className={
-      classNames('TodoInfo', { 'TodoInfo--completed': completed })}
-    >
-      <h2 className="TodoInfo__title">{title}</h2>
-      {user && <UserInfo user={user} />}
-    </article>
-  </>
+  <article className={
+    classNames('TodoInfo', { 'TodoInfo--completed': completed })}
+  >
+    <h2 className="TodoInfo__title">{title}</h2>
+    {user && <UserInfo user={user} />}
+  </article>
 );
