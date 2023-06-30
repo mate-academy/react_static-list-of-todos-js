4ee534814417f24aa2693f283c078dc25a758e2b
Nataliia Hrushanyk <133240440+NataliGru@users.noreply.github.com>
2023-06-30 09:40:02 +0300

    Update src/components/TodoInfo/TodoInfo.jsx
    Co-authored-by: AndyMan <epistolariy@ukr.net>

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -2,10 +2,10 @@ import { UserInfo } from '../UserInfo';
 import './TodoInfo.scss';
 
 export const TodoInfo = ({ todo: { completed, title, user } }) => {
-  const completedClass = completed ? ' TodoInfo--completed' : '';
+  const completedClass = completed ? 'TodoInfo--completed' : '';
 
   return (
-    <article className={`TodoInfo${completedClass}`}>
+    <article className={`TodoInfo ${completedClass}`}>
       <h2 className="TodoInfo__title">{title}</h2>
 
       {user && <UserInfo user={user} />}
