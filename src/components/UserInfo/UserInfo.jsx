4a8bf7b17ad5c1662692c2b308619a014ea634bd
Taras Bilas <tarasbilas01@gmail.com>
2023-06-30 03:57:21 +0300

    develop list of todos component

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,4 +1,12 @@
 // Add the required props
-export const UserInfo = () => (
-  <>UserInfo markup</>
-);
+export const UserInfo = ({ user }) => {
+  const { email, name } = user;
+
+  return (
+    <>
+      <a className="UserInfo" href={`mailto:${email}`}>
+        {name}
+      </a>
+    </>
+  );
+};
