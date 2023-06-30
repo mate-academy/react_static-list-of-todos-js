568726babd5770cd7989cdc979964c4578d02067
ihorhladkov <ihor.hladkov@gmail.com>
2023-06-30 10:41:19 +0300

    fixed

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,5 @@
 // Add the required props
+import cn from 'classnames';
 import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo }) => {
@@ -8,10 +9,14 @@ export const TodoInfo = ({ todo }) => {
     user,
   } = todo;
 
-  const isCompleted = completed ? 'TodoInfo TodoInfo--completed' : 'TodoInfo';
+  // const isCompleted = completed ? 'TodoInfo TodoInfo--completed' : 'TodoInfo';
 
   return (
-    <article className={isCompleted}>
+    // <article className={isCompleted}>
+    <article className={cn('TodoInfo', {
+      'TodoInfo--completed': completed,
+    })}>
+
       <h2 className="TodoInfo__title">{title}</h2>
       {user && <UserInfo user={user} />}
     </article>
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -3,6 +3,8 @@ import { TodoInfo } from '../TodoInfo/TodoInfo';
 // Add the required props
 export const TodoList = ({ todos }) => (
   <section className="TodoList">
-    {todos.map(todo => <TodoInfo todo={todo} key={todo.id} />)}
+    {todos.map(todo => (
+      <TodoInfo todo={todo} key={todo.id} />
+    ))}
   </section>
 );
