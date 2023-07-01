ae092d7ab0169897388aa11ed3b512e5ac984a2c
Daniil Postoiuk <daniil.postoyuk@gmail.com>
2023-07-01 12:30:21 +0300

    fixed requiered changes

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,8 +1,10 @@
+import cn from 'classnames';
 import { UserInfo } from '../UserInfo';
+import './TodoInfo.scss';
 
-// Add the required props
 export const TodoInfo = ({ todo }) => (
-  <article className={`TodoInfo ${todo.completed && ('TodoInfo--completed')}`}>
+  // eslint-disable-next-line
+  <article className={cn('TodoInfo', { 'TodoInfo--completed': todo.completed })}>
     <h2 className="TodoInfo__title">{todo.title}</h2>
 
     {todo.user && (<UserInfo user={todo.user} />)}
