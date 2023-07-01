cbeaf0a18512251adca54842039f97403bd7e7b5
Anton Baranskyi <antonbaranskij12@gmail.com>
2023-07-01 19:10:27 +0300

    use classnames

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,9 +1,17 @@
+import classNames from 'classnames';
 import { UserInfo } from '../UserInfo';
 
 // Add the required props
-export const TodoInfo = ({ todo: { completed, title, id, user } }) => (
-  <article className={`TodoInfo ${completed && 'TodoInfo--completed'}`}>
-    <h2 className="TodoInfo__title">{title}</h2>
-    {user && <UserInfo key={user.id} user={user} />}
-  </article>
-);
+export const TodoInfo = ({ todo: { completed, title, id, user } }) => {
+  const activeClass = classNames({
+    TodoInfo: true,
+    'TodoInfo--completed': completed,
+  });
+
+  return (
+    <article className={activeClass}>
+      <h2 className="TodoInfo__title">{title}</h2>
+      {user && <UserInfo key={user.id} user={user} />}
+    </article>
+  );
+};
