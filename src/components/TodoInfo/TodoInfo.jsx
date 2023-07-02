3bf7b95fea786c8728893ccb267c67c88dc89a87
Alexey Taranenko <alleksiuss99@gmail.com>
2023-07-02 15:29:35 +0300

    fixed

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import { UserInfo } from '../UserInfo';
+import './TodoInfo.scss';
 
 export const TodoInfo = ({ todo }) => {
   const {
@@ -9,13 +10,11 @@ export const TodoInfo = ({ todo }) => {
   } = todo;
 
   return (
-    <>
-      <article className={
-        classNames('TodoInfo', { 'TodoInfo--completed': completed })}
-      >
-        <h2 className="TodoInfo__title">{title}</h2>
-        {user && (<UserInfo user={user} />)}
-      </article>
-    </>
+    <article className={
+      classNames('TodoInfo', { 'TodoInfo--completed': completed })}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+      {user && (<UserInfo user={user} />)}
+    </article>
   );
 };
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,3 +1,5 @@
+import './UserInfo.scss';
+
 export const UserInfo = ({ user }) => {
   const {
     name,
