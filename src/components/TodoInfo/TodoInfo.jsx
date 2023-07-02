02549b09f6e6374a49444bd190c24143817901a4
Stanislav Vdovychenko <vdovychenkostas@gmail.com>
2023-07-02 20:45:46 +0300

    fix

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -3,13 +3,11 @@ import cn from 'classnames';
 import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo }) => (
-  <>
-    <article className={cn('TodoInfo', {
-      'TodoInfo--completed': todo.completed,
-    })}
-    >
-      <h2 className="TodoInfo__title">{`${todo.title}`}</h2>
-      <UserInfo user={todo.user} />
-    </article>
-  </>
+  <article className={cn('TodoInfo', {
+    'TodoInfo--completed': todo.completed,
+  })}
+  >
+    <h2 className="TodoInfo__title">{`${todo.title}`}</h2>
+    <UserInfo user={todo.user} />
+  </article>
 );
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -4,10 +4,9 @@ import { TodoInfo } from '../TodoInfo';
 export const TodoList = ({ todos }) => (
   <>
     {todos.map(todo => (
-      <section className="TodoList" ket={todo.id}>
+      <section className="TodoList" key={todo.id}>
         <TodoInfo todo={todo} />
       </section>
     ))}
-
   </>
 );
