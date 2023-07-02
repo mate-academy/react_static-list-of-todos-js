fb9e08c023223ece8eeeac3282f1b63dfaabe83c
outofs <rtranscendental@gmail.com>
2023-07-02 10:14:24 +0300

    Fixing classes for TodoInfo, adding separate scss files for components and fixing readability

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,7 +1,13 @@
+import cn from 'classnames';
+import './TodoInfo.scss';
 import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo }) => (
-  <article className={`TodoInfo ${todo.completed && 'TodoInfo--completed'}`}>
+  <article
+    className={
+      cn('TodoInfo', { 'TodoInfo--completed': todo.completed })
+    }
+  >
     <h2 className="TodoInfo__title">{todo.title}</h2>
     {todo.user && <UserInfo user={todo.user} />}
   </article>
