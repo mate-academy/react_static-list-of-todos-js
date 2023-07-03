c316ec5a0664a387490c3da9f48dff14e33db789
yurii-krymskiy <yurii.krymskyi@gmail.com>
2023-07-03 13:53:28 +0200

    new-sol

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,10 +1,19 @@
+import cn from 'classnames';
 import { UserInfo } from '../UserInfo';
 
-export const TodoInfo = ({ todo }) => (
-  <article className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}>
-    <h2 className="TodoInfo__title">
-      {todo.title}
-    </h2>
-    <UserInfo user={todo.user} />
-  </article>
-);
+export const TodoInfo = ({ todo }) => {
+  const { title, completed, user } = todo;
+
+  return (
+    <article className={cn('TodoInfo', {
+      'TodoInfo--completed': completed,
+    })}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      <UserInfo user={user} />
+    </article>
+  );
+};
