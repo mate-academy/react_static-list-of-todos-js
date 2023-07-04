d84ec7658b4a8e8845c45086edc7fcaedb0749ad
Artem Shuhailo <artem0462@gmail.com>
2023-07-04 17:26:52 +0300

    ready task

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,13 +1,17 @@
 import classNames from 'classnames';
 import { UserInfo } from '../UserInfo';
 
-export const TodoInfo = ({ todo }) => (
-  <article className={classNames('TodoInfo',
-    {
-      'TodoInfo--completed': todo.completed,
-    })}
-  >
-    <h2 className="TodoInfo__title">{todo.title}</h2>
-    {todo.user && <UserInfo user={todo.user} />}
-  </article>
-);
+export const TodoInfo = ({ todo }) => {
+  const { title, user, completed } = todo;
+
+  return (
+    <article className={classNames('TodoInfo',
+      {
+        'TodoInfo--completed': completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
