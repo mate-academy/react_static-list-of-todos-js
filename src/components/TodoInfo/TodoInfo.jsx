e869aebcfa9f3f6d5993e2975be24213f080d9f1
luka4ok <vitalik.shostak@gmail.com>
2023-07-05 11:30:03 +0300

    destructured props

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,9 +1,13 @@
 import { UserInfo } from '../UserInfo';
 import './TodoInfo.scss';
 
-export const TodoInfo = ({ todo }) => (
-  <article className={`TodoInfo ${todo.completed && `TodoInfo--completed`}`}>
-    <h2 className="TodoInfo__title">{todo.title}</h2>
-    {todo.user && <UserInfo user={todo.user} />}
-  </article>
-);
+export const TodoInfo = ({ todo }) => {
+  const { completed, title, user } = todo;
+
+  return (
+    <article className={`TodoInfo ${completed && `TodoInfo--completed`}`}>
+      <h2 className="TodoInfo__title">{title}</h2>
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
