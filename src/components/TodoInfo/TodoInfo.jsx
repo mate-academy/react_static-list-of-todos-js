053759535c99d64c23745bff0140b6f42cb54d41
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2023-07-06 17:50:19 +0300

    solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,19 @@
+import { UserInfo } from '../UserInfo';
+
 // Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+export const TodoInfo = ({ todo }) => {
+  function getClassName() {
+    if (todo.completed === true) {
+      return 'TodoInfo--completed';
+    }
+
+    return ' ';
+  }
+
+  return (
+    <article className={`TodoInfo ${getClassName(todo)}`}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      <UserInfo user={todo.user} />
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,12 @@
+import { TodoInfo } from '../TodoInfo';
+
 // Add the required props
-export const TodoList = () => (
-  <>TodoList markup</>
+export const TodoList = ({ todos }) => (
+  <section className="TodoList">
+    <div>
+      {todos.map(todo => (
+        <TodoInfo todo={todo} key={todo.id} />
+      ))}
+    </div>
+  </section>
 );
