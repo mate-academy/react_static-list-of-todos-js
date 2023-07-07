223784bf4193ff6e11da356f8484d2253ea3f84a
Vladyslav Filonenko <oladik05@gmail.com>
2023-07-07 22:07:06 +0300

    react-dotos

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,12 @@
-// Add the required props
-export const TodoList = () => (
-  <>TodoList markup</>
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
+export const TodoList = ({ todos }) => (
+  <>
+    {todos.map(todo => (
+      <section className="TodoList">
+        <TodoInfo todo={todo} key={todo.id} />
+      </section>
+    ))
+    }
+  </>
 );
