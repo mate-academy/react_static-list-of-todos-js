68fcb34a4cfd46003fa7ff4b65c19e4b35e70078
Yaroslav Dudar <dudar.komod@gmail.com>
2023-07-07 16:36:31 +0300

    Handle case when user is not exist was added

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -5,10 +5,10 @@ import { UserInfo } from '../UserInfo';
 export const TodoInfo = ({ todo }) => (
   <article
     className={
-    cn('TodoInfo', { 'TodoInfo--completed': todo.completed === true })
+    cn('TodoInfo', { 'TodoInfo--completed': todo.completed })
     }
   >
     <h2 className="TodoInfo__title">{todo.title}</h2>
-    <UserInfo user={todo.user} />
+    {todo.user && <UserInfo user={todo.user} />}
   </article>
 );
