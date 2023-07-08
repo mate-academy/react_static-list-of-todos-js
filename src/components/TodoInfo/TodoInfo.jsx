ae404a4658e3a1c4b8117fe3c11f38a9ec3b7e17
Dmytro Vovk <bellboy.dv@gmail.com>
2023-07-08 17:33:49 +0300

    Solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,15 @@
-// Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import { UserInfo } from '../UserInfo';
+
+export const TodoInfo = ({ todo }) => {
+  const { title, completed } = todo;
+
+  const articleClassName = `TodoInfo ${completed ? 'TodoInfo--completed' : ''}`;
+
+  return (
+    <article className={articleClassName}>
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      <UserInfo user={todo.user} />
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,4 +1,13 @@
-// Add the required props
-export const UserInfo = () => (
-  <>UserInfo markup</>
-);
+export const UserInfo = ({ user }) => {
+  if (!user) {
+    return null;
+  }
+
+  const { name, email } = user;
+
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
