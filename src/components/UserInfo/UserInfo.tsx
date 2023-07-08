3fed251fcc8d76d954a5aefe9d309aa8712fe790
Nikitin Evgeny <ngenya81@gmail.com>
2023-07-08 11:47:34 +0300

    message

---
+++ src/components/UserInfo/UserInfo.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { User } from '../../types/User';
+
+type Props = {
+  user: User;
+};
+
+export const UserInfo: React.FC<Props> = ({ user }) => {
+  const { name, email } = user;
+
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>{name}</a>
+  );
+};
