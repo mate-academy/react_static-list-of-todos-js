63497855ddeb2abf6fad09939c6e62a0ca87c0fa
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2023-07-08 18:14:51 +0300

    fixed

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -5,7 +5,7 @@ import { UserInfo } from '../UserInfo';
 export const TodoInfo = ({ todo }) => (
   <article
     className={cn('TodoInfo', {
-      'TodoInfo--completed': todo.completed === true,
+      'TodoInfo--completed': todo.completed,
     })}
   >
     <h2 className="TodoInfo__title">{todo.title}</h2>
