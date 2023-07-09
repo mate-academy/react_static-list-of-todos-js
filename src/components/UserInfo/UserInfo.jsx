81562f6bb37b8ae9fed2ffcce560b26c8ee70744
Eugene Kulish <genekulish13@gmail.com>
2023-07-09 13:18:54 +0300

    add TodoList, TodoInfo and UserInfo components

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,4 +1,9 @@
-// Add the required props
-export const UserInfo = () => (
-  <>UserInfo markup</>
+import './UserInfo.scss';
+
+export const UserInfo = ({ user }) => (
+  <>
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.username}
+    </a>
+  </>
 );
