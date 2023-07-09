11ce2fe45120db96ee534d1b4441edb2bf342ea4
Max <maxpusto533gmail.com>
2023-07-09 21:29:09 +0300

    repaired

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -4,12 +4,13 @@ import { UserInfo } from '../UserInfo';
 export const TodoInfo = ({ todo }) => {
   const {
     title,
+    user,
   } = todo;
 
   return (
     <article className="TodoInfo TodoInfo--completed">
       <h2 className="TodoInfo__title">{title}</h2>
-      {todo.user && <UserInfo user={todo.user} />}
+      {user && <UserInfo user={user} />}
     </article>
   );
 };
