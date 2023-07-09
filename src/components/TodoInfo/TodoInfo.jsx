55d95d8ea04fbe2877ad79a6a4527a8685ad9824
Eugene Kulish <genekulish13@gmail.com>
2023-07-09 17:51:32 +0300

    change operator

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -5,6 +5,6 @@ export const TodoInfo = ({ todo }) => (
   <article className={`TodoInfo${todo.completed === true ? ' TodoInfo--completed' : ''}`}>
     <h2 className="TodoInfo__title">{todo.title}</h2>
 
-    {todo.user ? <UserInfo user={todo.user} /> : null}
+    {todo.user && <UserInfo user={todo.user} />}
   </article>
 );
