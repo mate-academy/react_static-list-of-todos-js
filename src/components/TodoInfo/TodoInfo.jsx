20d7da813150058d38dd4d6e80ae6be600b6aab8
Eugene Kulish <genekulish13@gmail.com>
2023-07-09 17:14:02 +0300

    remove fragment

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -2,11 +2,9 @@ import { UserInfo } from '../UserInfo';
 import './TodoInfo.scss';
 
 export const TodoInfo = ({ todo }) => (
-  <>
-    <article className={`TodoInfo${todo.completed === true ? ' TodoInfo--completed' : ''}`}>
-      <h2 className="TodoInfo__title">{todo.title}</h2>
+  <article className={`TodoInfo${todo.completed === true ? ' TodoInfo--completed' : ''}`}>
+    <h2 className="TodoInfo__title">{todo.title}</h2>
 
-      {todo.user ? <UserInfo user={todo.user} /> : null}
-    </article>
-  </>
+    {todo.user ? <UserInfo user={todo.user} /> : null}
+  </article>
 );
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,9 +1,7 @@
 import './UserInfo.scss';
 
 export const UserInfo = ({ user }) => (
-  <>
-    <a className="UserInfo" href={`mailto:${user.email}`}>
-      {user.name}
-    </a>
-  </>
+  <a className="UserInfo" href={`mailto:${user.email}`}>
+    {user.name}
+  </a>
 );
