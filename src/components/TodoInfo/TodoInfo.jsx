f71131bca3c0b0aa047c59dd1834818034846274
Max <maxpusto533gmail.com>
2023-07-09 22:05:00 +0300

    className_added

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -5,10 +5,11 @@ export const TodoInfo = ({ todo }) => {
   const {
     title,
     user,
+    completed,
   } = todo;
 
   return (
-    <article className="TodoInfo TodoInfo--completed">
+    <article className={`TodoInfo ${completed ? 'TodoInfo--completed' : ''}`}>
       <h2 className="TodoInfo__title">{title}</h2>
       {user && <UserInfo user={user} />}
     </article>
