0a9c860301f3828881b074af8d44951fe4b364b0
grinjk1993 <grinjk1993@gmail.com>
2023-07-11 18:48:26 +0300

    bugs fixed

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -1,21 +1,23 @@
-describe('Page', () => {
+describe("Page", () => {
   beforeEach(() => {
-    cy.visit('/');
+    cy.visit("/");
   });
 
-  it('should render all the todos', () => {
-    cy.get('.TodoInfo').should('have.length', 200);
+  it("should render all the todos", () => {
+    cy.get(".TodoInfo").should("have.length", 200);
   });
 
-  it('should render todos in correct order', () => {
-    cy.get('.TodoInfo__title').eq(0).should('have.text', 'delectus aut autem');
-    cy.get('.TodoInfo__title').eq(199).should('have.text', 'ipsam aperiam voluptates qui');
+  it("should render todos in correct order", () => {
+    cy.get(".TodoInfo__title").eq(0).should("have.text", "delectus aut autem");
+    cy.get(".TodoInfo__title")
+      .eq(199)
+      .should("have.text", "ipsam aperiam voluptates qui");
   });
 
-  it('should render corresponding users', () => {
-    cy.get('.UserInfo').should('have.length', 200);
+  it("should render corresponding users", () => {
+    cy.get(".UserInfo").should("have.length", 200);
 
-    cy.get('.UserInfo').eq(0).should('have.text', 'Leanne Graham');
-    cy.get('.UserInfo').eq(199).should('have.text', 'Clementina DuBuque');
+    cy.get(".UserInfo").eq(0).should("have.text", "Leanne Graham");
+    cy.get(".UserInfo").eq(199).should("have.text", "Clementina DuBuque");
   });
 });
