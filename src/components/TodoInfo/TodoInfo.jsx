f1ba44b62e10f0f7325fe23e4cb245d8049bbdcb
Vitalii Hovenko <vet.hov.main@gmail.com>
2023-07-15 13:48:32 +0300

    rendered todo list

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,14 @@
-// Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+import { UserInfo } from '../UserInfo';
+
+export const TodoInfo = ({ todo }) => (
+  <>
+    <article className={todo.completed
+      ? 'TodoInfo TodoInfo--completed'
+      : 'TodoInfo'
+    }
+    >
+      <h1 className="TodoInfo__title">{todo.title}</h1>
+      <UserInfo user={todo.user} />
+    </article>
+  </>
 );
