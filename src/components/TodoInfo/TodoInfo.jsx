8369270651a392eda7d3635010f14b09ab4fd958
Anton Kabanets <treiser2010@gmail.com>
2023-07-19 18:08:33 +0300

    Fixed change classname on TodoInfo

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -4,25 +4,16 @@ export const TodoInfo = ({ todo }) => {
   const { completed, title, user } = todo;
 
   return (
-    (completed
-      ? (
-        <article className="TodoInfo TodoInfo--completed">
-          <h2 className="TodoInfo__title">{title}</h2>
 
-          <UserInfo
-            user={user}
-          />
-        </article>
-      )
-      : (
-        <article className="TodoInfo">
-          <h2 className="TodoInfo__title">{title}</h2>
+    <article className={completed
+      ? 'TodoInfo TodoInfo--completed'
+      : 'TodoInfo'}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
 
-          <UserInfo
-            user={user}
-          />
-        </article>
-      )
-    )
+      <UserInfo
+        user={user}
+      />
+    </article>
   );
 };
