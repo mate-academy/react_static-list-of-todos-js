fe5b03c0f4179a0e85ebb7a69d6b0d7119e24cb3
hnezbrytskyi <Heorhii.Nezbrytskyi@god.dev>
2023-07-19 17:43:15 +0300

    homework is done

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,14 @@
 // Add the required props
-export const TodoList = () => (
-  <>TodoList markup</>
+import { TodoInfo } from '../TodoInfo';
+
+export const TodoList = ({ todos }) => (
+  <section className="TodoList">
+    <div>
+      {
+        todos.map(todo => (
+          <TodoInfo todo={todo} key={todo.id} />
+        ))
+      }
+    </div>
+  </section>
 );
