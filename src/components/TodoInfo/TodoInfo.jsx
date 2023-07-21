451f0c5dd5f4892492436705abe48aef416303be
Antonina Klishch <tonya91t@gmail.com>
2023-07-21 23:15:50 +0300

    Dynamic class

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,7 +1,11 @@
+import cn from 'classnames';
 import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo }) => (
-  <article className={`TodoInfo ${todo.completed === true ? 'TodoInfo--completed' : ''}`}>
+  <article className={cn('TodoInfo', {
+    'TodoInfo--completed': todo.completed,
+  })}
+  >
     <h2 className="TodoInfo__title">{todo.title}</h2>
     <UserInfo user={todo.user} />
   </article>
