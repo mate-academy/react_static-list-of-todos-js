ef37513de47451fa1588833ebf8c19f25046d308
Volodymyr Petruliak <106393188+snuber-w@users.noreply.github.com>
2023-07-22 14:20:59 +0300

    second try

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -6,15 +6,17 @@ export const TodoInfo = ({ todo }) => {
   const { title, user, completed } = todo;
 
   return (
-    <article
-      className={classNames('TodoInfo',
-        { 'TodoInfo--completed': completed })}
-    >
-      <h2 className="TodoInfo__title">
-        {title}
-      </h2>
+    <>
+      <article
+        className={classNames('TodoInfo',
+          { 'TodoInfo--completed': completed })}
+      >
+        <h2 className="TodoInfo__title">
+          {title}
+        </h2>
 
-      <UserInfo user={user} />
-    </article>
+        <UserInfo user={user} />
+      </article>
+    </>
   );
 };
