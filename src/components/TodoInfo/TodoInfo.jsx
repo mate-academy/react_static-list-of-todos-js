795c44253b086c06369bc7b50c47490be1768628
Natalia Maryshko <n.maryshko@gmail.com>
2023-07-24 22:25:03 +0300

    fixed

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,10 +1,10 @@
 import { UserInfo } from '../UserInfo';
 import './TodoInfo.scss';
 
-export const TodoInfo = ({ todo }) => (
-  <article className={`TodoInfo${todo.completed ? ' TodoInfo--completed' : ''}`}>
-    <h2 className="TodoInfo__title">{todo.title}</h2>
+export const TodoInfo = ({ todo: { title, completed, user } }) => (
+  <article className={`TodoInfo${completed ? ' TodoInfo--completed' : ''}`}>
+    <h2 className="TodoInfo__title">{title}</h2>
 
-    {todo.user && <UserInfo user={todo.user} />}
+    {user && <UserInfo user={user} />}
   </article>
 );
