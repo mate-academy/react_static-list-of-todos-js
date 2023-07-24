0f34f84090ec33282346589f439415c982475efc
Dmytro Tymchenko <zav.pro_kr@ukr.net>
2023-07-24 11:58:14 +0300

    Fixed

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,12 +1,16 @@
+import cn from 'classnames';
 import './TodoInfo.scss';
 
 import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo }) => (
-  <article className={`TodoInfo ${todo.completed === true ? 'TodoInfo--completed' : ''}`}>
+  <article className={cn('TodoInfo', {
+    'TodoInfo--completed': todo.completed === true,
+  })}
+  >
     <h2 className="TodoInfo__title">{todo.title}</h2>
     {todo.user && (
-    <UserInfo user={todo.user} />
+      <UserInfo user={todo.user} />
     )}
   </article>
 );
