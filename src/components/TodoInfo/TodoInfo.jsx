97932d4ea438265433ce93e1ad9a9cc945954cbd
Nataliia <koreniuknataliia@gmail.com>
2023-07-24 21:12:07 +0300

    Solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,10 @@
 // Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+export const TodoInfo = ({ todo }) => (
+  <article className={`TodoInfo ${todo.completed === true ? 'TodoInfo-completed' : ''}`} key={todo.id}>
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+
+    <a className="UserInfo" href={todo.user.email}>
+      {todo.user.name}
+    </a>
+  </article>
 );
