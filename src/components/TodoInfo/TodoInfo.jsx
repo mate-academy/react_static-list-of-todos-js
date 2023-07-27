c16de15d8fbe045ddca271fa720c9c32d34b5432
Nataliia <koreniuknataliia@gmail.com>
2023-07-27 20:13:04 +0300

    added object destructing

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,10 +1,12 @@
-// Add the required props
-export const TodoInfo = ({ todo }) => (
-  <article className={`TodoInfo ${todo.completed === true ? 'TodoInfo-completed' : ''}`} key={todo.id}>
-    <h2 className="TodoInfo__title">{todo.title}</h2>
+import cn from 'classnames';
+import { UserInfo } from '../UserInfo';
 
-    <a className="UserInfo" href={todo.user.email}>
-      {todo.user.name}
-    </a>
+export const TodoInfo = ({ todo: { completed, title, user } }) => (
+  <article
+    className={cn('TodoInfo', { 'TodoInfo--completed': completed })}
+  >
+    <h2 className="TodoInfo__title">{title}</h2>
+
+    {user && (<UserInfo user={user} />)}
   </article>
 );
