b0cff5fa153fdd12aa46c084d89da4ef6fd9a1e8
OlegRak86 <rakolegit@gmail.com>
2023-07-30 11:39:58 +0300

    react_static-list-of-todos-js

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,15 @@
-// Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import { UserInfo } from '../UserInfo';
+
+export const TodoInfo = ({ todo }) => {
+  const { completed, title, user } = todo;
+
+  return (
+    <article className={`TodoInfo ${completed && 'TodoInfo--completed'}`}>
+      <h2 className="TodoInfo__title">{`${title}`}</h2>
+
+      <UserInfo
+        user={user}
+      />
+    </article>
+  );
+};
