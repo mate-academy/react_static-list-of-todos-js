9c16df5575dde7d6c4f93c0747043b02294db05e
tndeineka <tetianadeinekaa@gmail.com>
2023-07-31 18:14:58 +0300

    fixed solution

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,9 +1,15 @@
 import { TodoInfo } from '../TodoInfo';
+import './TodoList.scss';
 
 export const TodoList = ({ todos }) => (
   <section className="TodoList">
-    {todos.map(todo => (
-      <TodoInfo key={todo.id} todo={todo} />
-    ))}
+    <ul className="List">
+      {todos.map(todo => (
+        <li>
+          <TodoInfo key={todo.id} todo={todo} />
+        </li>
+      ))}
+    </ul>
+
   </section>
 );
