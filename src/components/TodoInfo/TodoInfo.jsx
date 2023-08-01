6e34d8847b32c2bff699370f7348e3807c24077b
dima_rogkov <dima.rogkov@cheitgroup.com>
2023-08-01 16:42:07 +0300

    some fix

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,14 +1,15 @@
+import cn from 'classnames';
 import { UserInfo } from '../UserInfo';
 import './TodoInfo.scss';
 
 export const TodoInfo = ({ todo }) => (
   <article
-    className={todo.completed ? 'TodoInfo TodoInfo--completed' : 'TodoInfo'}
+    className={cn('TodoInfo', { 'TodoInfo--completed': todo.completed })}
   >
     <h2 className="TodoInfo__title">
       {todo.title}
     </h2>
 
-    {todo.user ? <UserInfo user={todo.user} /> : null}
+    {todo.user && <UserInfo user={todo.user} />}
   </article>
 );
