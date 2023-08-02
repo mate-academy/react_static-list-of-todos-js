bb7cc6de354dc77767a6104a4ea16f907ea4bfe0
Aleksey-Aleksey <l0734062646@gmail.com>
2023-08-02 22:15:04 +0300

    fixed all your remarks

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -10,6 +10,9 @@ export const TodoInfo = ({ todo }) => (
     <h2 className="TodoInfo__title">
       {todo.title}
     </h2>
-    <UserInfo user={todo.user} />
+
+    {todo.user && (
+      <UserInfo user={todo.user} />
+    )}
   </article>
 );
