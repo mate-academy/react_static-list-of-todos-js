64ddb66ad7f64fdec3c4d91f55872fd46f24f45f
Mykhailo Shevchuk <mishashevchuk@gmail.com>
2023-08-03 15:07:38 +0300

    Solution

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,13 @@
+import { TodoInfo } from '../TodoInfo';
+
 // Add the required props
-export const TodoList = () => (
-  <>TodoList markup</>
+export const TodoList = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map(todo => (
+      <TodoInfo
+        todo={todo}
+        key={todo.key}
+      />
+    ))}
+  </section>
 );
