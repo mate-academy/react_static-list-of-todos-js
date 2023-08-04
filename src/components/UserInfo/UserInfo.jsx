9cdfcf315ec5bd1133832bfe84aac1c3e44961c3
Denys <kolbasin1990@gmail.com>
2023-08-04 20:24:07 +0300

    react_static-list-of-todos_v_01

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,4 +1,15 @@
-// Add the required props
-export const UserInfo = () => (
-  <>UserInfo markup</>
+export const UserInfo = ({
+  user: {
+    name,
+    email,
+  },
+}) => (
+  <a
+    className="UserInfo"
+    target="_blank"
+    rel="noreferrer"
+    href={`mailto:${email}`}
+  >
+    {name}
+  </a>
 );
