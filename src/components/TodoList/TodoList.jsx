1ea67a171ea6523bad5f781f2f7c5a6f49eb429d
RomanenkoStud <pavlo.romanenko@nure.ua>
2023-08-05 14:03:42 +0300

    add task solution

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,29 @@
-// Add the required props
-export const TodoList = () => (
-  <>TodoList markup</>
+import PropTypes from 'prop-types';
+import { TodoInfo } from '../TodoInfo';
+import './TodoList.scss';
+
+export const TodoList = ({ todos }) => (
+  <section className="TodoList">
+    <ul className="TodoList__list">
+      {todos.map(todo => (
+        <li key={todo.id} className="TodoList__item">
+          <TodoInfo todo={todo} />
+        </li>
+      ))}
+    </ul>
+  </section>
 );
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      completed: PropTypes.bool.isRequired,
+      title: PropTypes.string.isRequired,
+      user: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        email: PropTypes.string.isRequired,
+      }),
+    }),
+  ).isRequired,
+};
