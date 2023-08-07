959e5b19559fda00e0598c54d9b6ee966319d8ec
Oleksandr Chernikov <pjatochkin2013@gmail.com>
2023-08-07 12:00:49 +0300

    commit

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,13 @@
-// Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import React from 'react';
+import { UserInfo } from '../UserInfo';
+
+export const TodoInfo = ({ todo }) => {
+  const isCompleted = todo.completed;
+
+  return (
+    <article className={`TodoInfo${isCompleted ? ' TodoInfo--completed' : ''}`}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      {todo.user && <UserInfo user={todo.user} />}
+    </article>
+  );
+};
