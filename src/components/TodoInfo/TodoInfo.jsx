d0ca1b63a5fe7018c8c0fb5a3ff2644645960db6
Vasyl Zhyliakov <snak871@gmail.com>
2023-08-10 18:36:44 +0300

    Static list of TODOs

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,17 @@
-// Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+import cn from 'classnames';
+
+export const TodoInfo = (
+  { todo: { completed, title, user: { name, email } } },
+) => (
+  <article
+    className={cn('TodoInfo', {
+      'TodoInfo--completed': completed,
+    })}
+  >
+    <h2 className="TodoInfo__title">{title}</h2>
+
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  </article>
 );
