e3738caa413d7068abcdb07f4386c2428bc15c2d
Anna Shevtsova <aniashevtsova@gmail.com>
2023-08-15 14:39:20 +0200

    user existance checked

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -4,9 +4,9 @@ export const TodoInfo = ({ todo }) => {
   const { completed, title, user } = todo;
 
   return (
-    <article className={`TodoInfo${completed ? ' TodoInfo--completed' : ''}`}>
+    <article className={`TodoInfo ${completed ? ' TodoInfo--completed' : ''}`}>
       <h2 className="TodoInfo__title">{title}</h2>
-      <UserInfo user={user} />
+      { user && <UserInfo user={user} /> }
     </article>
   );
 };
