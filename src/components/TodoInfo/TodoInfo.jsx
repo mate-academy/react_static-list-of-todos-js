ae8b20ba0524322baa4611e3d3a47b94b4b0906b
Yuriy Bilskiy <yuriy.bilskiy@gmail.com>
2023-08-16 11:02:08 +0300

    fixed

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -11,7 +11,7 @@ export const TodoInfo = ({ todo }) => {
     >
       <h2 className="TodoInfo__title">{title}</h2>
 
-      <UserInfo user={user} />
+      {user && <UserInfo user={user} />}
     </article>
   );
 };
