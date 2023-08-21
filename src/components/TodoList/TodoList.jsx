de83db2f5b76bc8c5102a824ae627adbca35b7b7
Dmytro Levchenko <levchenkod91@gmail.com>
2023-08-21 14:10:29 +0300

    solution

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,16 @@
-// Add the required props
-export const TodoList = () => (
-  <>TodoList markup</>
+import { TodoInfo } from '../TodoInfo';
+
+export const TodoList = ({ todos }) => (
+  <ul
+    className="TodoList"
+    style={{
+      listStyle: 'none',
+      padding: 0,
+      margin: 0,
+    }}
+  >
+    {todos.map(todo => (
+      <TodoInfo todo={todo} key={todo.id} />
+    ))}
+  </ul>
 );
