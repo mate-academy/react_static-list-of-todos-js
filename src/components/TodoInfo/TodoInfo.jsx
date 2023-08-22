4b8dea2ea297c9c39bd2740fab2f3378f671bfaa
daviinnchi <zaikoivan99@gmail.com>
2023-08-22 21:36:39 +0100

    Refactored code

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -2,15 +2,13 @@ import classNames from 'classnames';
 import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo }) => (
-  <>
-    <article
-      className={classNames({
-        TodoInfo: true,
-        'TodoInfo--completed': todo.completed === true,
-      })}
-    >
-      <h2 className="TodoInfo__title">{todo.title}</h2>
-      <UserInfo user={todo.user} />
-    </article>
-  </>
+  <article
+    className={classNames({
+      TodoInfo: true,
+      'TodoInfo--completed': todo.completed === true,
+    })}
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+    <UserInfo user={todo.user} />
+  </article>
 );
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,15 +1,12 @@
 import { TodoInfo } from '../TodoInfo';
 
-// Add the required props
 export const TodoList = ({ todos }) => (
-  <>
-    <section className="TodoList">
-      {todos.map(todoTask => (
-        <TodoInfo
-          key={todoTask.id}
-          todo={todoTask}
-        />
-      ))}
-    </section>
-  </>
+  <section className="TodoList">
+    {todos.map(todoTask => (
+      <TodoInfo
+        key={todoTask.id}
+        todo={todoTask}
+      />
+    ))}
+  </section>
 );
