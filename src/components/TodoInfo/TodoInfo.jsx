bb167065aa675431252ed8c4ba23b9ceb073ca33
Andrii Kuz <andrykuz4556@gmail.com>
2023-08-22 16:15:55 +0300

    Some changes

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -5,7 +5,7 @@ export const TodoInfo = ({ todo }) => {
   const { title, completed, user } = todo;
 
   return (
-    <article className={`TodoInfo ${completed || 'TodoInfo--completed'}`}>
+    <article className={`TodoInfo ${completed && 'TodoInfo--completed'}`}>
       <h2 className="TodoInfo__title">{title}</h2>
       {user && <UserInfo user={user} />}
     </article>
