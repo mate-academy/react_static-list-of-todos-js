dab9c2dc2cac12ac90068d2a220e5d7d2237b60d
Ruslan Ihnatenko <ua.ignatenko@gmail.com>
2023-08-22 18:51:17 +0200

    Fix 2: removed all redundant fragments, removed redundant key property, used cn library

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -7,7 +7,7 @@ export const TodoInfo = ({ todo }) => {
   const { completed, title, user } = todo;
 
   return (
-    <article className={cn('TodoInfo', { 'TodoInfo--competed': completed })}>
+    <article className={cn('TodoInfo', { 'TodoInfo--completed': completed })}>
 
       <h2 className="TodoInfo__title">{title}</h2>
 
