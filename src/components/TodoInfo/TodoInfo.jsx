a9876cc7a0914d647240d1d8b0384a49a3adc781
Valentyn Bilyk <valentynbilyk22@gmail.com>
2023-08-23 15:50:05 +0300

    add destructuring, and classnames library

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,14 +1,26 @@
+import cn from 'classnames';
 import './TodoInfo.scss';
 import { UserInfo } from '../UserInfo';
 
-export const TodoInfo = ({ todo }) => (
-  <article className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : null}`}>
-    <h2 className="TodoInfo__title">
-      {todo.title}
-    </h2>
+export const TodoInfo = ({ todo }) => {
+  const {
+    title,
+    user,
+    completed,
+  } = todo;
 
-    {todo.user && (
-      <UserInfo user={todo.user} />
-    )}
-  </article>
-);
+  return (
+    <article className={cn('TodoInfo', {
+      'TodoInfo--completed': completed,
+    })}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      {user && (
+        <UserInfo user={user} />
+      )}
+    </article>
+  );
+};
