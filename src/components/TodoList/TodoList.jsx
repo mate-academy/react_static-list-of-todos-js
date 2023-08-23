737cdba5c1b874c7d09bd011d36ce1c04671b91f
Maksym <morenko.maxim@gmail.com>
2023-08-23 10:31:29 +0300

    refactor: change structure

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,7 +2,7 @@ import { TodoInfo } from '../TodoInfo';
 
 export const TodoList = ({ todos }) => (
   <section className="TodoList">
-    {todos && todos.map(todo => (
+    {todos?.map(todo => (
       <TodoInfo todo={todo} key={todo.id} />
     ))}
   </section>
