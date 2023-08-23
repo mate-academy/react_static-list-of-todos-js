aab8eb558cd2e5e0cfaffca0a040ddc9f9731801
Sergey <serg9205@gmail.com>
2023-08-23 23:13:46 +0300

    fix classname in todo info

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,11 +1,12 @@
 import './TodoInfo.scss';
+import cn from 'classnames';
 import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo }) => {
   const { title, completed, user } = todo;
 
   return (
-    <article className={`TodoInfo ${completed && `TodoInfo--completed`}`}>
+    <article className={cn('TodoInfo', { 'TodoInfo--completed': completed })}>
       <h2 className="TodoInfo__title">{title}</h2>
 
       {user && <UserInfo user={user} />}
