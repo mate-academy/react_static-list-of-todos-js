60d5dd14ab278df863e307041c47232c316d4d44
Oleh0293 <Oleh.kulyk.ik.2021@lpnu.ua>
2023-08-23 16:53:55 +0300

    Solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,21 @@
 // Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import CN from 'classnames';
+import { UserInfo } from '../UserInfo';
+
+import './TodoInfo.scss';
+
+export const TodoInfo = ({ todo }) => {
+  const {
+    title,
+    completed,
+    user,
+  } = todo;
+
+  return (
+    <article className={CN('TodoInfo', { 'TodoInfo--completed': completed })}>
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      {user && (<UserInfo user={user} />)}
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,4 +1,15 @@
 // Add the required props
-export const UserInfo = () => (
-  <>UserInfo markup</>
-);
+import './UserInfo.scss';
+
+export const UserInfo = ({ user }) => {
+  const {
+    email,
+    name,
+  } = user;
+
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
