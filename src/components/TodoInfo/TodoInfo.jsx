0bfbd3586f493826b8735250072c8fa97ae515e1
Roman Yatsykovych <55942779+yar0man@users.noreply.github.com>
2023-08-23 15:11:55 +0300

    add some changes

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -10,10 +10,9 @@ export const TodoInfo = ({ todo }) => {
         'TodoInfo--completed': completed,
       })}
     >
-
       <h2 className="TodoInfo__title">{title}</h2>
 
-      <UserInfo user={user} />
+      {user && (<UserInfo user={user} />)}
     </article>
   );
 };
