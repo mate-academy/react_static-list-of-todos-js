eb2cc41464ac732379576ddf38130de3441a9056
Viktoriia Roik <viktoriaroik2103@gmail.com>
2023-08-25 12:13:12 +0300

    add formatting

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -6,9 +6,16 @@ export const TodoInfo = ({ todo }) => {
   const { completed, title, user } = todo;
 
   return (
-    <article className={cn('TodoInfo', { 'TodoInfo--completed': completed })}>
-      <h2 className="TodoInfo__title">{title}</h2>
-      {user && (<UserInfo user={user} />)}
+    <article className={cn('TodoInfo', {
+      'TodoInfo--completed': completed,
+    })}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+      {user && (
+        <UserInfo user={user} />
+      )}
     </article>
   );
 };
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,10 +1,7 @@
 import './UserInfo.scss';
 
 export const UserInfo = ({ user }) => {
-  const {
-    name,
-    email,
-  } = user;
+  const { name, email } = user;
 
   return (
     <a className="UserInfo" href={`mailto:${email}`}>
