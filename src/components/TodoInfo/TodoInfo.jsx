385e06ff73c932a092174c31983b4b4ddbd1bd42
Anton <crossnaders4949@gmail.com>
2023-08-26 19:41:43 +0200

    Solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,13 @@
-// Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+import { UserInfo } from '../UserInfo';
+import './TodoInfo.scss';
+
+export const TodoInfo = ({ todo }) => (
+  <article className={`TodoInfo ${todo.completed === true ? 'TodoInfo--completed' : ''}`}>
+    <h2 className="TodoInfo__title">
+      {todo.title}
+    </h2>
+
+    <UserInfo user={todo.user} />
+  </article>
+
 );
