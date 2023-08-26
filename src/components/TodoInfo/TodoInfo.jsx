2891b5632220f5b3a48134613a8cc9114df51dbf
Serhii Shtokalo <Shtokalo1998@gmail.com>
2023-08-26 19:03:53 +0300

    fix all

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -2,14 +2,24 @@ import React from 'react';
 import classNames from 'classnames';
 import { UserInfo } from '../UserInfo';
 
-export const TodoInfo = ({ todo }) => (
-  <article
-    className={classNames('TodoInfo', {
-      'TodoInfo--completed': todo.completed,
-    })}
-  >
-    <h2 className="TodoInfo__title">{todo.title}</h2>
+export const TodoInfo = ({ todo }) => {
+  const { completed, title, user } = todo;
 
-    {todo.user && <UserInfo user={todo.user} />}
-  </article>
-);
+  // Визначте класи для елемента ul
+  const ulClassName = classNames('TodoInfo', {
+    'TodoInfo--completed': completed,
+  });
+
+  // Визначте класи для елемента li
+  const liClassName = classNames('TodoInfo__title', {
+    'TodoInfo__title--completed': completed,
+  });
+
+  return (
+    <article className={ulClassName}>
+      <h2 className={liClassName}>{title}</h2>
+
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
