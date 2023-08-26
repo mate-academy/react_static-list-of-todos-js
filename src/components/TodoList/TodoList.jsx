820ec58d5505675b3d585380c484a2bea6cff076
Levi199601 <udovichenkoivan16@gmail.com>
2023-08-26 13:55:19 -0700

    done

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,15 @@
-// Add the required props
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+import todosFromServer from '../../api/todos.json';
+
 export const TodoList = () => (
-  <>TodoList markup</>
+  <>
+    {todosFromServer.map(todo => (
+      <TodoInfo
+        title={todo.title}
+        tusk={todo.completed}
+        todosUserId={todo.userI
+      }
+      />
+    ))}
+  </>
 );
