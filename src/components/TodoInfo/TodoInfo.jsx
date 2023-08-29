053be76011afed41454e398a893360ed23cc0378
Bohdan Shumylo <injobs.kharkiv@gmail.com>
2023-08-29 20:40:41 +0300

    Fix Solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,12 +1,25 @@
 // Add the required props
+import classnames from 'classnames';
 import { UserInfo } from '../UserInfo';
 import './TodoInfo.scss';
 
+// export const TodoInfo = ({ todo }) => (
+//   <article className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}>
+//     <h2 className="TodoInfo__title">
+//       {todo.title}
+//     </h2>
+//     <UserInfo user={todo.user} />
+//   </article>
+// );
 export const TodoInfo = ({ todo }) => (
-  <article className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}>
-        <h2 className="TodoInfo__title">
-          {todo.title}
-        </h2>
-      <UserInfo user={todo.user} />
-      </article>
+  <article
+    className={classnames('TodoInfo', {
+      'TodoInfo--completed': todo.completed,
+    })}
+  >
+    <h2 className="TodoInfo__title">
+      {todo.title}
+    </h2>
+    <UserInfo user={todo.user} />
+  </article>
 );
