1afcd6ffc229bb58774ae8e8515daed6f8dfa017
UlianaChorna <110450537+UlianaChorna@users.noreply.github.com>
2023-08-31 16:19:52 +0300

    request change

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,11 +1,15 @@
 import classNames from 'classnames';
 import { UserInfo } from '../UserInfo/UserInfo';
 
-export const TodoInfo = ({ todo }) => (
-  <article className={classNames('TodoInfo',
-    { 'TodoInfo--completed': todo.completed })}
-  >
-    <h2 className="TodoInfo__title">{todo.title}</h2>
-    {todo.user && <UserInfo user={todo.user} /> }
-  </article>
-);
+export const TodoInfo = ({ todo }) => {
+  const { completed, title, user } = todo;
+
+  return (
+    <article className={classNames('TodoInfo',
+      { 'TodoInfo--completed': completed })}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+      {todo.user && <UserInfo user={user} /> }
+    </article>
+  );
+};
