d8350c464f3591141e297f0e2216cbbfd3c570f2
Nikita <cool.nikita734@gmail.com>
2023-09-01 16:19:03 +0200

    solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -2,15 +2,19 @@ import cn from 'classnames';
 import { UserInfo } from '../UserInfo';
 import './TodoInfo.scss';
 
-export const TodoInfo = ({ todo }) => (
-  <article className={cn('TodoInfo', {
-    'TodoInfo--completed': todo.completed === true,
-  })}
-  >
-    <h2 className="TodoInfo__title">
-      {todo.title}
-    </h2>
+export const TodoInfo = ({ todo }) => {
+  const { title, completed, user } = todo;
 
-    {todo.user && (<UserInfo user={todo.user} />)}
-  </article>
-);
+  return (
+    <article className={cn('TodoInfo', {
+      'TodoInfo--completed': completed === true,
+    })}
+    >
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
