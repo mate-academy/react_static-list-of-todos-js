d36a7020b013eaa29398dfef660065e5bd2c25fa
Vladyslav Kulakov <vladyslav@Vladyslavs-MacBook-Pro.local>
2023-09-02 19:06:46 +0300

    refactor changes

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,10 +1,10 @@
+import classNames from 'classnames';
 import { UserInfo } from '../UserInfo';
 import './TodoInfo.scss';
 
 export const TodoInfo = ({ todo }) => (
-  <article className={`TodoInfo ${todo.completed
-    ? 'TodoInfo--completed'
-    : ''}`}
+  <article className={classNames('TodoInfo',
+    { 'TodoInfo--completed': todo.completed })}
   >
     <h2 className="TodoInfo__title">{todo.title}</h2>
     {todo.user && (<UserInfo user={todo.user} />)}
