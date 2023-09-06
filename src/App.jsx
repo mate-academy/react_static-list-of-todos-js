a176fe4982d34681c283d774cf15fcc5050265f6
Andrii Rykal <andreuka79@gmail.com>
2023-09-06 15:24:06 +0300

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -2,79 +2,21 @@ import './App.scss';
 
 import todosFromServer from './api/todos.json';
 import usersFromServer from './api/users.json';
+import { TodoList } from './components/TodoList';
 
 function getUserById(userId) {
   return usersFromServer.find(user => user.id === userId)
     || null;
 }
 
-export const todos = todosFromServer.map(todo => ({
+const todos = todosFromServer.map(todo => ({
   ...todo,
   user: getUserById(todo.userId),
 }));
-
-const UserInfo = ({ user }) => (
-  <a className="UserInfo" href="mailto:Sincere@april.biz">
-    {user.name}
-  </a>
-);
-
-export const TodoInfo = ({ todo }) => (
-  <article className="TodoInfo TodoInfo--completed">
-    <h2 className="TodoInfo__title">HTML</h2>
-
-    <UserInfo user={todo.user} />
-  </article>
-);
-
-const TodoList = ({ todos }) => (
-  <section className="TodoList">
-    {todos.map(todo => (
-      <TodoInfo todo={todo} key={todo.id} />
-    ))}
-  </section>
-);
 
 export const App = ({ user }) => (
   <div className="App">
     <h1 className="App__title">Static list of todos</h1>
     <TodoList todos={todos} />
-
-    <section className="TodoList">
-      {/* <TodoInfo todo={todo} /> */}
-      {/* <article className="TodoInfo TodoInfo--completed">
-        <h2 className="TodoInfo__title">HTML</h2>
-
-        <UserInfo />
-
-        <a className="UserInfo" href="mailto:Sincere@april.biz">
-          Leanne Graham
-        </a>
-      </article> */}
-
-      <article className="TodoInfo TodoInfo--completed">
-        <h2 className="TodoInfo__title">CSS</h2>
-
-        <a className="UserInfo" href="mailto:Sincere@april.biz">
-          Leanne Graham
-        </a>
-      </article>
-
-      <article className="TodoInfo TodoInfo--completed">
-        <h2 className="TodoInfo__title">JS</h2>
-
-        <a className="UserInfo" href="mailto:Shanna@melissa.tv">
-          Ervin Howell
-        </a>
-      </article>
-
-      <article className="TodoInfo">
-        <h2 className="TodoInfo__title">React</h2>
-
-        <a className="UserInfo" href="mailto:Nathan@yesenia.net">
-          Clementine Bauch
-        </a>
-      </article>
-    </section>
   </div>
 );
