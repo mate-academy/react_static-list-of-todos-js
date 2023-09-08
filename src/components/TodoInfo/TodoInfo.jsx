cc87665228766adf4d2dcf7e03c8467bd1aacf0a
Leonid Krainik <rialleons@gmail.com>
2023-09-08 07:28:21 +0300

    Create react static list of TODOs

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,14 @@
-// Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+import { UserInfo } from '../UserInfo';
+import './TodoInfo.scss';
+
+export const TodoInfo = ({
+  todo: { completed, title, user },
+}) => (
+  <article className={`TodoInfo ${completed ? 'TodoInfo--completed' : ''}`}>
+    <h2 className="TodoInfo__title">{title}</h2>
+
+    {!!user && (
+      <UserInfo user={user} />
+    )}
+  </article>
 );
