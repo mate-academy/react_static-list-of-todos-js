63f7f7b4923c817baf5432ba72d24fe8f23c2aa7
Yaroslav Prisyazhniy <prisyazhniy2002@gmail.com>
2023-09-12 21:01:07 +0300

    task solution added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.scss';
-
 import todosFromServer from './api/todos.json';
 import usersFromServer from './api/users.json';
 import { TodoList } from './components/TodoList';
@@ -18,6 +17,5 @@ export const App = () => (
   <div className="App">
     <h1 className="App__title">Static list of todos</h1>
     <TodoList todos={todos} />
-
   </div>
 );
--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,8 +1,12 @@
-import { UserInfo } from "../UserInfo";
+import cn from 'classnames';
+import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo }) => (
-  <article className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}>
+  <article className={cn('TodoInfo', {
+    'TodoInfo--completed': todo.completed === true,
+  })}
+  >
     <h2 className="TodoInfo__title">{todo.title}</h2>
-    <UserInfo user={todo.user} />
+    {todo.user && <UserInfo user={todo.user} />}
   </article>
 );
