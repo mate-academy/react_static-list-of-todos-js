a0b2e455ac1ccb59fabc1717f81cbb2614108d1b
Ivan Baranovskyi <103584840+ivan-baranovskyi@users.noreply.github.com>
2023-09-14 12:07:23 +0200

    made list

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,11 @@
-// Add the required props
-export const TodoList = () => (
-  <>TodoList markup</>
+import { TodoInfo } from '../TodoInfo';
+
+export const TodoList = ({ todos, users }) => (
+  <section className="TodoList">
+    {todos.map(todo => (
+      <article key={todo.id}>
+        <TodoInfo todo={todo} user={todo.user} key={todo.user.id} />
+      </article>
+    ))}
+  </section>
 );
