b5261dc911abf036360ea905194aba6afd2f63a1
Ivan Baranovskyi <103584840+ivan-baranovskyi@users.noreply.github.com>
2023-09-14 12:47:37 +0200

    fix all mistakes

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -3,9 +3,7 @@ import { TodoInfo } from '../TodoInfo';
 export const TodoList = ({ todos, users }) => (
   <section className="TodoList">
     {todos.map(todo => (
-      <article key={todo.id}>
-        <TodoInfo todo={todo} user={todo.user} key={todo.user.id} />
-      </article>
+      <TodoInfo todo={todo} user={todo.user} key={todo.id} />
     ))}
   </section>
 );
