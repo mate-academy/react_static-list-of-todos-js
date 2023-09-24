6af9809dece740a65f266b48c8e9e3c54dbdc40f
mark <hachevskyy@gmail.com>
2023-09-24 19:47:45 +0200

    Solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -3,7 +3,7 @@ import './UserInfo.scss';
 export const UserInfo = ({ user }) => (
   <>
     {user && (
-      <a className="UserInfo" href={user.email}>
+      <a className="UserInfo" href={`mailto:${user.email}`}>
         {user.name}
       </a>
     )}
