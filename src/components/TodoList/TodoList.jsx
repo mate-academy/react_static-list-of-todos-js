e39b9bf6339e80463e23be695865cfb0896e0fe6
George Savostikov <georgesavostikov@gmail.com>
2023-09-24 17:16:24 +0300

    Solution

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,9 @@
-// Add the required props
-export const TodoList = () => (
-  <>TodoList markup</>
-);
+import { TodoInfo } from '../TodoInfo';
+
+export const TodoList = ({ todos }) => {
+  const result = todos.map(todo => (
+    <TodoInfo todo={todo} />
+  ));
+
+  return result;
+};
