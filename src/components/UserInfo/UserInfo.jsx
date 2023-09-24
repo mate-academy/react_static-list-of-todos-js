4f7f3e5d85e2b5e5b632d149416fdc67064dcd4f
mark <hachevskyy@gmail.com>
2023-09-24 19:43:12 +0200

    Solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,4 +1,11 @@
-// Add the required props
-export const UserInfo = () => (
-  <>UserInfo markup</>
+import './UserInfo.scss';
+
+export const UserInfo = ({ user }) => (
+  <>
+    {user && (
+      <a className="UserInfo" href={user.email}>
+        {user.name}
+      </a>
+    )}
+  </>
 );
