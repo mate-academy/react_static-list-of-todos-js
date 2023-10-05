abf4c752124b529970c1f9bcc3968179750339e2
Illia Drach <illyadrach@gmail.com>
2023-10-05 16:27:40 +0300

    implement task solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,25 @@
-// Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import { UserInfo } from '../UserInfo';
+import usersFromServer from '../../api/users.json';
+
+function getUserById(userId) {
+  return usersFromServer.find(user => user.id === userId)
+      || null;
+}
+
+export const TodoInfo = ({
+  todo: {
+    userId,
+    completed,
+    title,
+  },
+}) => {
+  const user = getUserById(userId);
+
+  return (
+    <article className={`TodoInfo ${completed === true ? 'TodoInfo--completed' : ''}`}>
+      <h2 className="TodoInfo__title">{`${title}`}</h2>
+
+      <UserInfo user={user} />
+    </article>
+  );
+};
