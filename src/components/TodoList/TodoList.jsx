e3551c4fa5929096287a7f68e360f50f323acdf1
Dmytro Panin <dmitriyp1598@gmail.com>
2023-10-08 16:06:37 +0300

    add task solution

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,14 @@
-// Add the required props
-export const TodoList = () => (
-  <>TodoList markup</>
+import { TodoInfo } from '../TodoInfo';
+import { UserInfo } from '../UserInfo';
+
+export const TodoList = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map(todo => (
+      <TodoInfo todo={todo} todoId={todo.id} />
+    ))}
+
+    {/* {todos.map(todo => (
+      <UserInfo todo={todo} todoId={todo.id} />
+    ))} */}
+  </section>
 );
