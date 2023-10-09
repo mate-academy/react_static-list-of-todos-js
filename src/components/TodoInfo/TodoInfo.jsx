cb6ea32f8d6c4e1f621dc000aa2279ac50fb9ca9
getsi <getsize1144@gmail.com>
2023-10-09 19:36:39 +0300

    Completed the task

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,19 @@
 // Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+import { UserInfo } from '../UserInfo';
+import usersFromServer from '../../api/users.json';
+
+function getUserById(userId) {
+  return <UserInfo user={usersFromServer.find(user => user.id === userId)} />
+      || null;
+}
+
+export const TodoInfo = ({ todo }) => (
+
+  <article className={`TodoInfo ${todo.completed && 'TodoInfo--completed'}`}>
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+
+    {getUserById(todo.userId)}
+  </article>
 );
+
+// TodoInfo--completed
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,8 @@
 // Add the required props
-export const TodoList = () => (
-  <>TodoList markup</>
+
+import { TodoInfo } from '../TodoInfo';
+
+export const TodoList = ({ todos }) => (
+  todos.map(todo => <TodoInfo todo={todo} key={todo.id} />)
+
 );
