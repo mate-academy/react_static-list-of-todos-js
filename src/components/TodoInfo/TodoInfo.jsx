72907d001bb2d2703e8e983ed5ebe0c453715471
Zhekich123 <axelb380@gmail.com>
2023-10-10 14:44:04 +0300

    bug fix

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -2,11 +2,9 @@ import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo }) => (
   <div>
-    <article className="TodoInfo">
-      <div className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}>
-        <h2 className="TodoInfo__title">{todo.title}</h2>
-        {todo.user && <UserInfo user={todo.user} key={todo.id} />}
-      </div>
+    <article className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      {todo.user && <UserInfo user={todo.user} key={todo.id} />}
     </article>
   </div>
 );
