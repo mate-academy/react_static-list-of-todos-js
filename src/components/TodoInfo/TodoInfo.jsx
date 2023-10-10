4112b0569d75f282979e8439b8dbf2b9f5865dce
Zhekich123 <axelb380@gmail.com>
2023-10-10 13:41:04 +0300

    make todo list

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,12 @@
-// Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+import { UserInfo } from '../UserInfo';
+
+export const TodoInfo = ({ todo }) => (
+  <div>
+    <article className="TodoInfo">
+      <div className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}>
+        <h2 className="TodoInfo__title">{todo.title}</h2>
+        {todo.user && <UserInfo user={todo.user} key={todo.id} />}
+      </div>
+    </article>
+  </div>
 );
