29d7438503544fe2b8c87995ccf6d2bd600739f9
andrew gidrovich <mr.androvich55555@gmail.com>
2023-10-15 13:23:29 +0300

    add task solution

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,16 @@
-// Add the required props
-export const TodoList = () => (
-  <>TodoList markup</>
-);
+import React from 'react';
+import { TodoInfo } from '../TodoInfo';
+
+export const TodoList = ({ todos }) => {
+  if (!todos || todos.length === 0) {
+    return <div className="TodoList">No todos to display.</div>;
+  }
+
+  return (
+    <section className="TodoList">
+      {todos.map(({ id, ...todo }) => (
+        <TodoInfo key={id} todo={todo} />
+      ))}
+    </section>
+  );
+};
