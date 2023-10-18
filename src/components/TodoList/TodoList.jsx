d412b5b865020d6f9bb0b01c1e4720b1d00f85fa
Danylo Artomov <daniilart01@gmail.com>
2023-10-18 23:03:21 +0300

    implementation

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,21 @@
-// Add the required props
+import todosFromServer from '../../api/todos.json';
+import usersFromServer from '../../api/users.json';
+import { TodoInfo } from '../TodoInfo';
+
+function getUserById(userId) {
+  return usersFromServer.find(user => user.id === userId)
+      || null;
+}
+
+const todos = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUserById(todo.userId),
+}));
+
 export const TodoList = () => (
-  <>TodoList markup</>
+  <section className="TodoList">
+    {todos.map(todo => (
+      <TodoInfo todo={todo} key={todo.id} />
+    ))}
+  </section>
 );
