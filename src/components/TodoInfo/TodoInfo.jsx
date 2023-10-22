f984f23d7c9080e7937fd2a852b6bea2e1f23704
Kateryna Sydorenko <katerynnnna@gmail.com>
2023-10-22 18:29:23 +0300

    task solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,20 @@
-// Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import classNames from 'classnames';
+import { UserInfo } from '../UserInfo';
+
+export const TodoInfo = ({ todo }) => {
+  const { completed, user, title } = todo;
+
+  const isCompleted = classNames('TodoInfo', {
+    'TodoInfo--completed': completed,
+  });
+
+  return (
+    <article className={isCompleted}>
+      <h2 className="TodoInfo__title">
+        {title}
+      </h2>
+
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
