0b9f45c4f7d28a5b0781b28b70b1eac49ee1d6fe
Ivan Orinchak <124816409+IvanOrinchak@users.noreply.github.com>
2023-10-28 10:27:15 +0300

    add task solution

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,14 @@
-// Add the required props
-export const TodoList = () => (
-  <>TodoList markup</>
+import { TodoInfo } from '../TodoInfo';
+
+export const TodoList = ({ todos }) => (
+  <>
+    <section className="TodoList">
+      {todos.map(listCh => (
+        <TodoInfo
+          todo={listCh}
+          key={listCh.id}
+        />
+      ))}
+    </section>
+  </>
 );
