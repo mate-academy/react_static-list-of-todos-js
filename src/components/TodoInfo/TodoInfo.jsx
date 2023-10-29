3304b0bd2be0abac4b20002bb648ca2b6731b9e5
Denys Vasylenkoe <kapodenim@gmail.com>
2023-10-29 10:53:08 +0200

    v1

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,17 @@
 // Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+import { UserInfo } from '../UserInfo/UserInfo';
+
+function completeTodo(completed) {
+  if (completed) {
+    return `TodoInfo--completed`;
+  }
+
+  return '';
+}
+
+export const TodoInfo = ({ todo }) => (
+  <article className={`TodoInfo ${completeTodo(todo.completed)}`}>
+    <h2 className="TodoInfo__title">{`${todo.title}`}</h2>
+    <UserInfo user={todo.user} />
+  </article>
 );
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,4 +1,14 @@
 // Add the required props
-export const UserInfo = () => (
-  <>UserInfo markup</>
+function checkUser(user) {
+  if (user !== undefined) {
+    return `${user}`;
+  }
+
+  return '';
+}
+
+export const UserInfo = ({ user }) => (
+  <a className="UserInfo" href={checkUser(user.email)}>
+    {checkUser(user.name)}
+  </a>
 );
