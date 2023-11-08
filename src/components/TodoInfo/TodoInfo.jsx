042b637abc390fe81b9b723276d18c7b3fa6a679
Nazar Levkovych <nazar.levkovich.2018@gmail.com>
2023-11-08 21:16:41 +0200

    fix v1.1

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -2,13 +2,17 @@ import cl from 'classnames';
 import { UserInfo } from '../UserInfo';
 import './TodoInfo.scss';
 
-export const TodoInfo = ({ todo }) => (
-  <article className={cl('TodoInfo', {
-    'TodoInfo--completed': todo.completed,
-  })}
-  >
-    <h2 className="TodoInfo__title">{todo.title}</h2>
+export const TodoInfo = ({ todo }) => {
+  const { title, completed, user } = todo;
 
-    <UserInfo user={todo.user} />
-  </article>
-);
+  return (
+    <article className={cl('TodoInfo', {
+      'TodoInfo--completed': completed,
+    })}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      <UserInfo user={user} />
+    </article>
+  );
+};
