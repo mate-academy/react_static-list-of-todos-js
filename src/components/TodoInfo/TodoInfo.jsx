1ada66d9a9c6a3c179bc12268630bb4ca8823157
Artem Sydun <sydun2003@gmail.com>
2023-11-08 19:58:06 +0200

    develop

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,24 @@
 // Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import './TodoInfo.scss';
+import cl from 'classnames';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+export const TodoInfo = ({ todo }) => {
+  const { title, completed, user } = todo;
+
+  return (
+    <>
+      <article className={cl('TodoInfo', {
+        'TodoInfo--completed': completed,
+      })}
+      >
+
+        <h2 className="TodoInfo__title">
+          {title}
+        </h2>
+
+        <UserInfo user={user} />
+      </article>
+    </>
+  );
+};
