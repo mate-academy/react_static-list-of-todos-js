1531b72c6f1602253dd659e6543a0349f9ddf986
Malyholovka Ostap <maligolovkaostap@gmail.com>
2023-11-18 21:08:11 +0200

    add task solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,14 @@
+import { UserInfo } from '../UserInfo';
+
 // Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+export const TodoInfo = ({ todo }) => {
+  const completed = todo.completed ? 'TodoInfo--completed' : '';
+
+  return (
+    <article className={`TodoInfo ${completed}`}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      <UserInfo user={todo.user} />
+    </article>
+  );
+};
