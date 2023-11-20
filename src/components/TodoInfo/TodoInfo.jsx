3dddc4654faed76b7254ee8de123a9ba14004dc2
Anton Kuchmasov <antek.ukraine@gmail.com>
2023-11-20 18:38:24 +0200

    done task static-list-of-todos-js

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,24 @@
 // Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+import { UserInfo } from '../UserInfo/UserInfo';
+import usersFromServer from '../../api/users.json';
+import todosFromServer from '../../api/todos.json';
+
+function getUserById(userId) {
+  return usersFromServer.find(user => user.id === userId)
+      || null;
+}
+
+export const todos = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUserById(todo.userId),
+}));
+
+export const TodoInfo = ({ todo }) => (
+  <article className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}>
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+    <UserInfo
+      key={getUserById(todo.userId).id}
+      user={getUserById(todo.userId)}
+    />
+  </article>
 );
