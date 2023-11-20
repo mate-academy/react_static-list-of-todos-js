0da8cbac0292dff9653e9b2318267956a49bc581
Sergey Emelyanov <mrleondono@gmail.com>
2023-11-20 22:06:17 +0200

    react_todo-list task solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,20 @@
-// Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import { UserInfo } from '../UserInfo';
+
+export const TodoInfo = ({ todo }) => {
+  const { title, user, completed } = todo;
+  const ifCompleted = completed ? 'TodoInfo--completed' : '';
+
+  return (
+    <>
+      <article className={`TodoInfo ${ifCompleted}`}>
+        <h2 className="TodoInfo__title">{title}</h2>
+
+        {user !== undefined
+          ? (
+            <UserInfo user={user} />
+          )
+          : <></>}
+      </article>
+    </>
+  );
+};
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,42 @@
+import { TodoInfo } from '../TodoInfo';
+
 // Add the required props
-export const TodoList = () => (
-  <>TodoList markup</>
+export const TodoList = ({ todos }) => (
+  <>
+    <section className="TodoList">
+      {todos.map(todo => <TodoInfo todo={todo} />)}
+
+      {/* <article className="TodoInfo TodoInfo--completed">
+        <h2 className="TodoInfo__title">HTML</h2>
+
+        <a className="UserInfo" href="mailto:Sincere@april.biz">
+          Leanne Graham
+        </a>
+      </article>
+
+      <article className="TodoInfo TodoInfo--completed">
+        <h2 className="TodoInfo__title">CSS</h2>
+
+        <a className="UserInfo" href="mailto:Sincere@april.biz">
+          Leanne Graham
+        </a>
+      </article>
+
+      <article className="TodoInfo TodoInfo--completed">
+        <h2 className="TodoInfo__title">JS</h2>
+
+        <a className="UserInfo" href="mailto:Shanna@melissa.tv">
+          Ervin Howell
+        </a>
+      </article>
+
+      <article className="TodoInfo">
+        <h2 className="TodoInfo__title">React</h2>
+
+        <a className="UserInfo" href="mailto:Nathan@yesenia.net">
+          Clementine Bauch
+        </a>
+      </article> */}
+    </section>
+  </>
 );
