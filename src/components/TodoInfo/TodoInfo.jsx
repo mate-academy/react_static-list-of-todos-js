38fa83eac17f4cd41e33f5bf32f18aa9d47a8a56
Oleksandr Kulibaba <alks.aesthetic@gmail.com>
2023-11-30 12:19:44 +0200

    solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,21 @@
-// Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+import cn from 'classnames';
+import { UserInfo } from '../UserInfo';
+import './TodoInfo.scss';
+
+export const TodoInfo = ({ todo: {
+  id,
+  title,
+  completed,
+  user,
+} }) => (
+  <li className={cn('TodoInfo', {
+    'TodoInfo--completed': completed,
+  })}
+  >
+    <h2 className="TodoInfo__title">
+      {title}
+    </h2>
+
+    {user && <UserInfo user={user} />}
+  </li>
 );
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,9 @@
-// Add the required props
-export const TodoList = () => (
-  <>TodoList markup</>
+import { TodoInfo } from '../TodoInfo';
+
+export const TodoList = ({ todos }) => (
+  <ul className="TodoList">
+    {todos.map(todo => (
+      <TodoInfo key={todo.id} todo={todo} />
+    ))}
+  </ul>
 );
