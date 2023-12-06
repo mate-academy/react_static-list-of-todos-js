306de58ff9f60b5091e2241b22cb0ff1e74571c0
Heorhii Churhuliia <gio4929189@gmail.com>
2023-12-06 18:23:54 +0000

    add task solution

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,7 +2,7 @@ import { TodoInfo } from '../TodoInfo';
 
 export const TodoList = ({ todos }) => (
   <div className="TodoList">
-    {todos.map(todo => (
+    {todos && todos.map(todo => (
       <TodoInfo key={todo.id} todo={todo} />
     ))}
   </div>
