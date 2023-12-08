e5d2e896ef85b26865851b6e00f057387bdf5dfe
boytrend0108 <boytrend@gmail.com>
2023-12-08 11:44:24 +0200

    step

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,19 @@
 // Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import cd from 'classnames';
+import './TodoInfo.scss';
+import { UserInfo } from '../UserInfo';
+
+export const TodoInfo = ({ todo }) => {
+  const { title, user, completed } = todo;
+
+  return (
+    <article className={cd('TodoInfo', {
+      'TodoInfo--completed': completed,
+    })}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      {user && <UserInfo todo={todo} />}
+    </article>
+  );
+};
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,4 +1,14 @@
 // Add the required props
-export const UserInfo = () => (
-  <>UserInfo markup</>
-);
+export const UserInfo = ({ todo }) => {
+  const { name, username, email } = todo.user;
+
+  return (
+    <div>
+      <p>{name}</p>
+      <p>{username}</p>
+      <a className="UserInfo" href="mailto:Sincere@april.biz">
+        {email}
+      </a>
+    </div>
+  );
+};
