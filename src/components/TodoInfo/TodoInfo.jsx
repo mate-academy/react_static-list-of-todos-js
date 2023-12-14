ac462a2d100fc9868b187b9f23c4a90882575a78
Kyryb <kyrybcode@gmail.com>
2023-12-14 12:07:50 +0200

    TodoList created

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,16 @@
-// Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import { UserInfo } from '../UserInfo';
+
+export const TodoInfo = ({ todo }) => {
+  const className = `TodoInfo${todo.completed
+    ? ' TodoInfo--completed' : ''}`;
+
+  return (
+    <article className={className}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      {todo.user
+        ? (<UserInfo user={todo.user} />)
+        : null}
+    </article>
+  );
+};
