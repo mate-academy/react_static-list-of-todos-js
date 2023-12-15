810fc63a6de3ed0e5ef9d7fff0b705d2d847b654
Volodymyr Baraban <barabanv2003@gmail.com>
2023-12-15 19:34:20 +0200

    react todos

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,17 @@
-// Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import { UserInfo } from '../UserInfo';
+import './TodoInfo.scss';
+
+export const TodoInfo = ({ todo }) => {
+  const { completed, title, user } = todo;
+  const isCompleted = completed === true ? '--completed' : '';
+
+  return (
+    <>
+      <article className={`TodoInfo TodoInfo${isCompleted}`}>
+        <h2 className="TodoInfo__title">{title}</h2>
+
+        <UserInfo user={user} />
+      </article>
+    </>
+  );
+};
