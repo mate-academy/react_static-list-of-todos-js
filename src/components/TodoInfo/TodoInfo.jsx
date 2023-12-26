43baa5ad0fe55e9a361463bed7417624df9c26be
Julia <julia@ocuhealthcare.com>
2023-12-26 14:51:23 -0600

    Solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,19 @@
-// Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import { UserInfo } from '../UserInfo';
+
+export const TodoInfo = ({ todo }) => {
+  const articleClass = todo.completed
+    ? 'TodoInfo TodoInfo--completed'
+    : 'TodoInfo';
+
+  return (
+    <article className={articleClass}>
+
+      <h2 className="TodoInfo__title">
+        {todo.title}
+      </h2>
+
+      <UserInfo user={todo.user} key={todo.user.id} />
+
+    </article>
+  );
+};
