becddfdee2f0ffef744ccc75bd06de8a4cb23623
Andrew Rich <andy.rich3a@gmail.com>
2024-01-03 18:21:45 +0200

    add task solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,13 @@
+import { UserInfo } from '../UserInfo';
+
 // Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+export function TodoInfo({ todo }) {
+  const completedModifier = todo.completed ? ' TodoInfo--completed' : '';
+
+  return (
+    <article className={`TodoInfo${completedModifier}`}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      <UserInfo user={todo.user} />
+    </article>
+  );
+}
