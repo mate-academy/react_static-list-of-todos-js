fd926abc8a57a24cbae37ff32b197198f17af816
Maksym Kalashnyk <maxplay123890@gmail.com>
2024-01-09 00:22:00 +0200

    Split `App` component into separate files following the React task guideline. (generated by blackbox.ai )

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,12 @@
-// Add the required props
-export const TodoList = () => (
-  <>TodoList markup</>
+import React from 'react';
+import TodoInfo from './components/TodoInfo/TodoInfo';
+
+const TodoList = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map(todo => (
+      <TodoInfo key={todo.id} todo={todo} />
+    ))}
+  </section>
 );
+
+export default TodoList;
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,4 +1,10 @@
-// Add the required props
-export const UserInfo = () => (
-  <>UserInfo markup</>
+// UserInfo.jsx
+import React from 'react';
+
+const UserInfo = ({ user }) => (
+  <a className="UserInfo" href={`mailto:${user.email}`}>
+    {user.name}
+  </a>
 );
+
+export default UserInfo;
