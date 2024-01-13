2c5315bf06187581bbeab32d78117c2243822fa2
Oleh Khrystofor <hristoforolegivanovich@gmail.com>
2024-01-13 21:19:11 +0200

    add mailto1

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,6 +1,6 @@
 // Add the required props
 export const UserInfo = ({ todo }) => (
-  <a className="UserInfo" href={`mailto: + ${todo.user.email}`}>
+  <a className="UserInfo" href={`mailto:${todo.user.email}`}>
     {todo.user.username}
   </a>
 );
