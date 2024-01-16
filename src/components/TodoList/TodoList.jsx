599d5bdf0b7c567688bf352ba17937a16cd54d88
kovachhh <koval.viktor338@gmail.com>
2024-01-16 23:40:44 +0200

    Implemented solution

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,12 @@
-// Add the required props
-export const TodoList = () => (
-  <>TodoList markup</>
+import { TodoInfo } from '../TodoInfo/index';
+
+export const TodoList = ({ todos }) => (
+  <>
+    <h1 className="App__title">Static list of todos</h1>
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo todo={todo} key={todo.id} />
+      ))}
+    </section>
+  </>
 );
