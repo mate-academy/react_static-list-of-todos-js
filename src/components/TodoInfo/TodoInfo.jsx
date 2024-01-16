480d4485a58e90ffad0a1cff7408720e3eb4bbf5
Viktoriia Melnyk <melnychka1993@gmail.com>
2024-01-16 00:06:29 +0000

    done

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,13 @@
+import cn from 'classnames';
+import { UserInfo } from '../UserInfo';
+
 // Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
+export const TodoInfo = ({ todo }) => (
+  <article
+    className={cn('TodoInfo',
+      { 'TodoInfo--completed': todo.completed === true })}
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+    <UserInfo user={todo.user} />
+  </article>
 );
