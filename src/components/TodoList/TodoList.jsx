8e527b8d163da2d3e0003b528d9f9436f3cf972a
Anna <annashcheglova.06.08@gmail.com>
2024-01-26 20:14:10 +0200

    Solution

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,42 @@
 // Add the required props
-export const TodoList = () => (
-  <>TodoList markup</>
+import { TodoInfo } from '../TodoInfo';
+
+export const TodoList = ({ todos }) => (
+  // <>TodoList markup</>
+  <section className="TodoList">
+    {todos.map(todo => (
+      // <article className="TodoInfo TodoInfo--completed">
+      //   <h2 className="TodoInfo__title">HTML</h2>
+
+      //   <a className="UserInfo" href="mailto:Sincere@april.biz">
+      //     Leanne Graham
+      //   </a>
+      // </article>
+      <TodoInfo todo={todo} />
+    ))}
+
+    {/* <article className="TodoInfo TodoInfo--completed">
+      <h2 className="TodoInfo__title">CSS</h2>
+
+      <a className="UserInfo" href="mailto:Sincere@april.biz">
+        Leanne Graham
+      </a>
+    </article>
+
+    <article className="TodoInfo TodoInfo--completed">
+      <h2 className="TodoInfo__title">JS</h2>
+
+      <a className="UserInfo" href="mailto:Shanna@melissa.tv">
+        Ervin Howell
+      </a>
+    </article>
+
+    <article className="TodoInfo">
+      <h2 className="TodoInfo__title">React</h2>
+
+      <a className="UserInfo" href="mailto:Nathan@yesenia.net">
+        Clementine Bauch
+      </a>
+    </article> */}
+  </section>
 );
