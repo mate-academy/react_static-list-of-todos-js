e6172cd6507bbf7f47169800379a89a53839148c
alona <alionazhv@gmail.com>
2024-02-08 12:23:13 +0100

    The UserInfo, TodoInfo and TodoList components were completed

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,15 @@
-// Add the required props
-export const TodoInfo = () => (
-  <>TodoInfo markup</>
-);
+import { UserInfo } from '../UserInfo';
+import usersFromServer from '../../api/users.json';
+
+const TodoInfo = ({ todo }) => {
+  const person = usersFromServer.find(user => user.id === todo.userId);
+
+  return (
+    <article className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      {person && <UserInfo user={person} />}
+    </article>
+  );
+};
+
+export { TodoInfo };
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,4 +1,11 @@
-// Add the required props
-export const TodoList = () => (
-  <>TodoList markup</>
+import { TodoInfo } from '../TodoInfo';
+
+const TodoList = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map(todo => (
+      <TodoInfo key={todo.id} todo={todo} />
+    ))}
+  </section>
 );
+
+export { TodoList };
