da66098ad96882dc8351309cae4e12e3298d026d
Kostiantyn Shestakov <kostiashestakov10@gmail.com>
2024-02-17 00:04:47 +0200

    solution fix

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -4,7 +4,10 @@ import { TodoInfo } from '../TodoInfo';
 export const TodoList = ({ todos }) => (
   <section className="TodoList">
     {todos.map(todo => (
-      <TodoInfo todo={todo} />
+      <TodoInfo
+        key={todo.id}
+        todo={todo}
+      />
     ))}
   </section>
 );
