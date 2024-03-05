a8e90932d4dd9468d880ce9dd9366a3de662e349
Dmytro Haidash <tipocto.ckujiji@gmail.com>
2024-03-05 22:45:06 +0200

    9/11

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,8 +1,11 @@
+import cn from 'classnames';
 import { UserInfo } from '../UserInfo/UserInfo';
 import users from '../../api/users.json';
 
 export const TodoInfo = ({ todo }) => (
-  <article className="TodoInfo">
+  <article
+    className={cn('TodoInfo', { 'TodoInfo--completed': todo.completed })}
+  >
     <h2 className="TodoInfo__title">{todo.title}</h2>
     <UserInfo user={users[todo.userId]} />
   </article>
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,6 +1,6 @@
 export const UserInfo = ({ user }) =>
   user && (
-    <a className="UserInfo" href={user.email}>
+    <a className="UserInfo" href={`mailto:${user.email}`}>
       {user.name}
     </a>
   );
