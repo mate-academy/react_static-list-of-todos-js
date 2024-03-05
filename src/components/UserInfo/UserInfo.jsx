4e72caf9c6a4fbf9a2c71b7182b919c1707b85e4
illya-onyshchuk <llayonys@gmail.com>
2024-03-05 14:15:23 +0200

    add solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,2 +1,12 @@
 // Add the required props
-export const UserInfo = () => <>UserInfo markup</>;
+export const UserInfo = ({ user }) => {
+  // console.log(user.email);
+
+  return (
+    user && (
+      <a className="UserInfo" href={`mailto:${user.email}`}>
+        {user.name}
+      </a>
+    )
+  );
+};
