0c4c30fb440a156a63edb3b510f6dee37c8a0bf1
Dmitriy Luhasnkiy <luhanskiydmitriy@gmail.com>
2024-03-06 10:56:05 +0300

    update solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,8 +1,8 @@
 import { UserInfo } from '../UserInfo';
 
-export const TodoInfo = ({ todo }) => {
+export const TodoInfo = ({ todo }) => (
   <article className={`TodoInfo ${todo.completed && 'TodoInfo--completed'}`}>
     <h2 className="TodoInfo__title">{todo.title}</h2>
     {todo.user && <UserInfo user={todo.user} key={todo.user.id} />}
-  </article>;
-};
+  </article>
+);
