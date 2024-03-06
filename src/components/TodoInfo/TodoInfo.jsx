9c1559758c792cec78fccf47f29a1af399481246
Taras Nechyporuck <tarasnechyporuck@gmail.com>
2024-03-06 17:05:32 +0200

    task done

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,20 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import React from 'react';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+import users from '../../api/users.json';
+
+export const TodoInfo = ({ todo }) => {
+  const user = users.find(userFrom => userFrom.id === todo.userId);
+
+  const className = todo.completed
+    ? 'TodoInfo TodoInfo--completed'
+    : 'TodoInfo';
+
+  return (
+    <article className={className}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      <UserInfo user={user} key={user.id} />
+    </article>
+  );
+};
