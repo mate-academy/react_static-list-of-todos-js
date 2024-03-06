7db43babc5bc6f0789cbf691fbcfd6cebd9e58a3
Maksym <max2112gereta@gmail.com>
2024-03-06 15:38:02 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import './App.scss';
+
+import todosFromServer from './api/todos.json';
+import usersFromServer from './api/users.json';
 import { TodoList } from './components/TodoList/TodoList';
-import todos from './api/todos.json';
+
+function getUserById(userId) {
+  return usersFromServer.find(user => user.id === userId) || null;
+}
+
+export const todos = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUserById(todo.userId),
+}));
 
 export const App = () => (
   <div className="App">
     <h1 className="App__title">Static list of todos</h1>
+
     <TodoList todos={todos} />
   </div>
 );
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,5 +1,6 @@
-export const UserInfo = ({ user }) => (
-  <a className="UserInfo" href={`mailto:${user.email}`}>
-    {user.name}
-  </a>
-);
+export const UserInfo = ({ user }) =>
+  user && (
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
+  );
