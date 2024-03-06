4f1672ddc40d182298beeadc0a39919a8ef2a866
Dmitriy Luhasnkiy <luhanskiydmitriy@gmail.com>
2024-03-06 10:29:54 +0300

    update solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,7 +1,5 @@
-export const UserInfo = ({ user }) => {
-  <>
-    <a className="UserInfo" href={user.email}>
-      {user.name}
-    </a>
-  </>;
-};
+export const UserInfo = ({ user }) => (
+  <a className="UserInfo" href={`mailto:${user.email}`}>
+    {user.name}
+  </a>
+);
