1b503b2f422fceabc47a69a58b91e95909367d41
Renat Abdrakhmanov <renatabdrah001@gmail.com>
2024-03-07 21:18:50 +0200

    1 test failed

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,14 @@
+import { UserInfo } from '../UserInfo/UserInfo';
+
 // Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+export const TodoInfo = ({ todo }) => (
+  <article
+    className={`TodoInfo ${
+      todo.completed === true ? 'TodoInfo--completed' : null
+    }`}
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+
+    <UserInfo user={todo} />
+  </article>
+);
