1e4865975fb8800998db070806c0a8a97202261f
Stanislav Kukharuk <kukharuk.stan1slav@gmail.com>
2024-03-13 17:23:23 +0000

    v: 1

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,17 +1,13 @@
 import cn from 'classnames';
+import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo }) => {
   const completed = todo.completed === true;
 
   return (
-    todo.user && (
-      <article className={cn('TodoInfo', { 'TodoInfo--completed': completed })}>
-        <h2 className="TodoInfo__title">{todo.title}</h2>
-
-        <a className="UserInfo" href={`mailto:${todo.user.email}`}>
-          {todo.user.name}
-        </a>
-      </article>
-    )
+    <article className={cn('TodoInfo', { 'TodoInfo--completed': completed })}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      {todo.user && <UserInfo user={todo.user} />}
+    </article>
   );
 };
