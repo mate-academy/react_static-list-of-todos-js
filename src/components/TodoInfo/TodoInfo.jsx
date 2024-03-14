1f4eed3487b614acb20b6cf9dc15440e0837c346
MaximAV <atanasovmaksim56@gmail.com>
2024-03-14 21:28:38 +0200

    solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,16 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import React from 'react';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+export const TodoInfo = ({ todo, users }) => {
+  const className = todo.completed
+    ? 'TodoInfo TodoInfo--completed'
+    : 'TodoInfo';
+
+  return (
+    <article className={className}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      {users && <UserInfo user={users.find(user => user.id === todo.userId)} />}
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,10 @@
-// Add the required props
-export const TodoList = () => <>TodoList markup</>;
+import React from 'react';
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
+export const TodoList = ({ todos, users }) => {
+  return todos.map(todo => (
+    <section className="TodoList">
+      <TodoInfo key={todo.id} todo={todo} users={users} />
+    </section>
+  ));
+};
