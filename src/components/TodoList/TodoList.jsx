c745ab17bf450b4bb2ba6aa02c7fb5d6462f995f
beliyandr <beliyandr@gmail.com>
2024-04-14 23:06:50 +0300

    first variant

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,26 @@
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
 // Add the required props
-export const TodoList = () => <>TodoList markup</>;
+export const TodoList = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map(todo => (
+      <TodoInfo todo={todo} key={todo.id} />
+    ))}
+
+    {/* <article className="TodoInfo TodoInfo--completed">
+    <h2 className="TodoInfo__title">HTML</h2>
+
+    <a className="UserInfo" href="mailto:Sincere@april.biz">
+      Leanne Graham
+    </a>
+  </article>
+
+  <article className="TodoInfo">
+    <h2 className="TodoInfo__title">React</h2>
+
+    <a className="UserInfo" href="mailto:Nathan@yesenia.net">
+      Clementine Bauch
+    </a>
+  </article> */}
+  </section>
+);
