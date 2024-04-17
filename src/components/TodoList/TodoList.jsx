79e86c3dc3800ebed903e9fc8af514f18c449017
denys <denys.sheremeta.web@gmail.com>
2024-04-17 22:45:19 +0300

    solution

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,15 @@
-// Add the required props
-export const TodoList = () => <>TodoList markup</>;
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
+export const TodoList = ({ todos }) => {
+  return (
+    <>
+      {todos.length > 0 && (
+        <ul>
+          {todos.map(el => {
+            return <TodoInfo key={el.id} todo={el} />;
+          })}
+        </ul>
+      )}
+    </>
+  );
+};
