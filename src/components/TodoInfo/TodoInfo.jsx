e5b520937acff79985368d57d07269135e34a17a
Vladislav <7evenLays@gmail.com>
2024-04-17 15:01:35 +0300

    todolist Done

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,18 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import { UserInfo } from '../UserInfo/UserInfo';
+
+export const TodoInfo = ({ todo }) => {
+  let todoInfoClassStatus = 'TodoInfo';
+  const completed = '--completed';
+
+  if (todo.completed) {
+    todoInfoClassStatus += completed;
+  }
+
+  return (
+    <article className={`TodoInfo ${todoInfoClassStatus}`}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      <UserInfo user={todo.user} />
+    </article>
+  );
+};
