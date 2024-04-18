bfd2f768ede60f25a651479191daf1ef0610bfff
denys <denys.sheremeta.web@gmail.com>
2024-04-18 10:43:29 +0300

    using cn lib

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,12 +1,12 @@
+import cn from 'classnames';
 import { UserInfo } from '../UserInfo/UserInfo';
 
 export const TodoInfo = ({ todo }) => {
   const { title, user, completed } = todo;
-  const articleClass = completed ? 'TodoInfo TodoInfo--completed' : 'TodoInfo';
 
   return (
     <li>
-      <article className={articleClass}>
+      <article className={cn('TodoInfo', { 'TodoInfo--completed': completed })}>
         <h2 className="TodoInfo__title">{title}</h2>
 
         <UserInfo user={user} />
