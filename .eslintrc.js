0c5afa2688bb5f6139b70cfa30b47a54d8b4e296
Marina Semipyatnaya <epicentr50@gmail.com>
2024-04-21 12:26:29 +0300

    add task solution

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   extends: ['@mate-academy/eslint-config-react', 'plugin:cypress/recommended'],
   rules: {},
+
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  },
 };
