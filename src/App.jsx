ca78e4a61d65e95154d99e62bd3818818278bff3
Greshch <greshchook@gmail.com>
2024-04-23 14:36:49 +0300

    decomposition was done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.scss';
 
 import todosFromServer from './api/todos.json';
 import usersFromServer from './api/users.json';
-// import { TodoInfo } from './components/TodoInfo/TodoInfo';
+import { TodoList } from './components/TodoList/TodoList';
 
 function getUserById(userId) {
   return usersFromServer.find(user => user.id === userId) || null;
@@ -13,86 +13,9 @@ export const todos = todosFromServer.map(todo => ({
   user: getUserById(todo.userId),
 }));
 
-export const UserInfo = ({ user }) => (
-  <a className="UserInfo" href={user.email}>
-    {user.name}
-  </a>
-);
-
-export const TodoInfo = ({ todo }) => {
-  const completed = todo.completed ? ' TodoInfo--completed' : '';
-
-  return (
-    <article className={`TodoInfo${completed}`}>
-      <h2 className="TodoInfo__title">{todo.title}</h2>
-
-      {/* <a className="UserInfo" href={todo.user.email}>
-        {todo.user.name}
-      </a> */}
-      <UserInfo user={todo.user} />
-    </article>
-  );
-};
-
-export const TodoList = ({ todosList }) => (
-  <section className="TodoList">
-    {/* {console.log(todos)} */}
-    {/* {todosList.map(todo => {
-      const completed = todo.completed ? ' TodoInfo--completed' : '';
-
-      return (
-        <article key={todo.id} className={`TodoInfo${completed}`}>
-          <h2 className="TodoInfo__title">{todo.title}</h2>
-
-          <a className="UserInfo" href={todo.user.email}>
-            {todo.user.name}
-          </a>
-        </article>
-      );
-    })} */}
-    {todosList.map(todo => (
-      <TodoInfo key={todo.id} todo={todo} />
-    ))}
-  </section>
-);
-
 export const App = () => (
   <div className="App">
     <h1 className="App__title">Static list of todos</h1>
-
-    {/* <section className="TodoList">
-      <article className="TodoInfo TodoInfo--completed">
-        <h2 className="TodoInfo__title">HTML</h2>
-
-        <a className="UserInfo" href="mailto:Sincere@april.biz">
-          Leanne Graham
-        </a>
-      </article>
-
-      <article className="TodoInfo TodoInfo--completed">
-        <h2 className="TodoInfo__title">CSS</h2>
-
-        <a className="UserInfo" href="mailto:Sincere@april.biz">
-          Leanne Graham
-        </a>
-      </article>
-
-      <article className="TodoInfo TodoInfo--completed">
-        <h2 className="TodoInfo__title">JS</h2>
-
-        <a className="UserInfo" href="mailto:Shanna@melissa.tv">
-          Ervin Howell
-        </a>
-      </article>
-
-      <article className="TodoInfo">
-        <h2 className="TodoInfo__title">React</h2>
-
-        <a className="UserInfo" href="mailto:Nathan@yesenia.net">
-          Clementine Bauch
-        </a>
-      </article>
-    </section> */}
-    <TodoList todosList={todos} />
+    <TodoList todos={todos} />
   </div>
 );
