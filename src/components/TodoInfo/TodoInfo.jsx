8aeb624abd383656d98ded43924d76b31bb2d1de
Arthur Fujii <arthurfujii@gmail.com>
2024-04-24 18:23:52 -0300

    first commit

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,13 @@
+import { UserInfo } from '../UserInfo/UserInfo';
+
 // Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+export const TodoInfo = ({ todo }) => (
+  <article
+    className={
+      todo.completed === true ? 'TodoInfo TodoInfo--completed' : 'TodoInfo'
+    }
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+    {todo.user !== null ? <UserInfo user={todo.user} key={todo.userId} /> : ''}
+  </article>
+);
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,20 @@
-// Add the required props
-export const TodoList = () => <>TodoList markup</>;
+import todosFromServer from '../../api/todos.json';
+import usersFromServer from '../../api/users.json';
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
+function getUserById(userId) {
+  return usersFromServer.find(user => user.id === userId) || null;
+}
+
+export const todos = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUserById(todo.userId),
+}));
+
+export const TodoList = () => (
+  <section className="TodoList">
+    {todos.map(todo => (
+      <TodoInfo todo={todo} key={todo.id} />
+    ))}
+  </section>
+);
