354de17de0204611aba992c6ba8cc94dc1c3751a
Dmytro Bashlai <imaxs2016@gmail.com>
2024-05-16 22:22:04 +0300

    The react static list of todos have been created

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,9 @@
-// Add the required props
-export const TodoList = () => <>TodoList markup</>;
+import { TodoInfo } from '../TodoInfo';
+
+export const TodoList = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map(todoInfo => (
+      <TodoInfo todo={todoInfo} title={todoInfo.title} key={todoInfo.id} />
+    ))}
+  </section>
+);
