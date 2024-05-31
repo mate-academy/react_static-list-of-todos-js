c8111c13fb23b5205a89720163b4483460e9a586
unknown <pyuliya2016@gmail.com>
2024-05-31 07:16:36 +0300

    Solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,11 +1,9 @@
 import { UserInfo } from '../UserInfo/UserInfo';
 
-export const TodoInfo = ({ todo, users }) => (
+export const TodoInfo = ({ todo }) => (
   <article className={`TodoInfo ${todo.completed && 'TodoInfo--completed'}`}>
     <h2 className="TodoInfo__title">{todo.title}</h2>
 
-    {users.find(user => user.id === todo.userId) && (
-      <UserInfo user={users.find(user => user.id === todo.userId)} />
-    )}
+    {todo.user && <UserInfo user={todo.user} />}
   </article>
 );
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,11 +1,13 @@
 import { TodoInfo } from '../TodoInfo/TodoInfo';
 
 // Add the required props
-export const TodoList = ({ todos, users }) => (
-  <section className="TodoList">
-    {todos.map(todo => (
-      <TodoInfo todo={todo} users={users} key={todo.id} />
-    ))}
-    ;
-  </section>
-);
+export const TodoList = ({ todos }) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo todo={todo} key={todo.id} />
+      ))}
+      ;
+    </section>
+  );
+};
