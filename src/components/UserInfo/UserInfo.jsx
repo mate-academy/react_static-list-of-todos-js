7fee97523de879f41daced141bd0390052a635ad
Seva Podolskiy <s_podolskiy@live.com>
2024-05-31 10:10:19 -0400

    solitioni

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,2 +1,8 @@
+import React from 'react';
+
 // Add the required props
-export const UserInfo = () => <>UserInfo markup</>;
+export const UserInfo = ({ user }) => (
+  <a className="UserInfo" href={`mailto:${user.email}`}>
+    {user.name}
+  </a>
+);
