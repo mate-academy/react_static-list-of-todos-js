b45ac0eac85a38ff6cdbce1a1cd37d6083dd5c43
Les Dyman <lesdyman@gmail.com>
2024-06-10 19:30:58 +0200

    add solution for the static list of todos task

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,13 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import React from 'react';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+export const TodoInfo = ({ todo }) => {
+  const { title, user, completed } = todo;
+
+  return (
+    <article className={`TodoInfo ${completed ? 'TodoInfo--completed' : ''}`}>
+      <h2 className="TodoInfo__title">{title}</h2>
+      <UserInfo user={user} />
+    </article>
+  );
+};
