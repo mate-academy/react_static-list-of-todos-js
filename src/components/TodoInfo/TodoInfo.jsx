fa7e53fa9aba7d5d3691089cfd38dc5b241451bb
Stanislav Kushnir <florian.of.lorch@gmail.com>
2024-06-11 18:25:30 +0300

    completed static list of todos

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,11 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import { UserInfo } from '../UserInfo';
+
+export const TodoInfo = ({ todo: { user, completed, title } }) => (
+  <article
+    className={`TodoInfo ${completed === true && 'TodoInfo--completed'}`}
+  >
+    <h2 className="TodoInfo__title">{title}</h2>
+
+    <UserInfo user={user} />
+  </article>
+);
