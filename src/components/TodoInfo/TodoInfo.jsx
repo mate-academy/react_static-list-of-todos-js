c9fc493f2a29898d740b86d83c00ad31a93b8238
MarkGodnyi <libertyinprogresslife@gmail.com>
2024-06-17 15:42:27 +0200

    created todo list

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,14 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import './TodoInfo.scss';
+import { UserInfo } from '../UserInfo';
+
+export const TodoInfo = ({ todo }) => {
+  const todoCompleted = `TodoInfo + ${todo.completed && ' TodoInfo--completed'}`;
+
+  return (
+    <article className={todoCompleted}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      <UserInfo user={todo.user} />
+    </article>
+  );
+};
