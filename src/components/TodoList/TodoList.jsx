070ef1916fcf57e2cee951e8d60fa6ae46789181
Mykhal <misha.soroka.49@gmail.com>
2024-07-19 13:35:33 +0200

    BUG FIXING

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,9 @@
-// Add the required props
-export const TodoList = () => <>TodoList markup</>;
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
+export const TodoList = ({ todos }, { users }) => (
+  <section className="TodoList">
+    {todos.map(todo => (
+      <TodoInfo todos={todos} users={users} key={todo.id} />
+    ))}
+  </section>
+);
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,2 +1,9 @@
-// Add the required props
-export const UserInfo = () => <>UserInfo markup</>;
+export const UserInfo = ({ users }, todo) => (
+  <>
+    {users.map(user => (
+      <a className="UserInfo" href={user.email} key={user.id}>
+        {`${user.id === todo.userId ? user.name : ''}`}
+      </a>
+    ))}
+  </>
+);
