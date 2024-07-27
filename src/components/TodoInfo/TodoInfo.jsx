d1544b1a9fc8ccf838424d66cb30d3da02c8f564
RimmaBuzuluk <rimmysya2000@gmail.com>
2024-07-27 11:59:13 +0300

    add modificator

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,14 +1,11 @@
 import { UserInfo } from '../UserInfo/UserInfo';
 
-// Add the required props
 export const TodoInfo = ({ todo }) => {
-  // console.log(todos);
   const { title, user } = todo;
 
   return (
     <article className="TodoInfo">
-      {/* <p>{todos.title}</p> */}
-      <h2 className="TodoInfo__title">{title}</h2>
+      <h2 className="TodoInfo__title TodoInfo--completed">{title}</h2>
       {user && <UserInfo user={user} />}
     </article>
   );
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,6 +1,5 @@
 export const UserInfo = ({ user }) => {
   const { name, email } = user;
-  // consolr.log(user);
 
   return (
     <a className="UserInfo" href={email}>
