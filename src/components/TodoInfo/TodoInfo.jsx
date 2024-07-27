4bde4fde33182b436d4b75318c99b894bf32ebad
RimmaBuzuluk <rimmysya2000@gmail.com>
2024-07-27 12:11:52 +0300

    add TodoInfo--completed

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -4,8 +4,8 @@ export const TodoInfo = ({ todo }) => {
   const { title, user } = todo;
 
   return (
-    <article className="TodoInfo">
-      <h2 className="TodoInfo__title TodoInfo--completed">{title}</h2>
+    <article className="TodoInfo TodoInfo--completed">
+      <h2 className="TodoInfo__title ">{title}</h2>
       {user && <UserInfo user={user} />}
     </article>
   );
