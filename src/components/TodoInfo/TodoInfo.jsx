cc142ced996a09ff60da53d36543dc0325ecaa37
RimmaBuzuluk <rimmysya2000@gmail.com>
2024-07-27 12:21:28 +0300

    add completed fror modificator

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,10 +1,13 @@
 import { UserInfo } from '../UserInfo/UserInfo';
 
 export const TodoInfo = ({ todo }) => {
-  const { title, user } = todo;
+  const { id, completed, title, user } = todo;
 
   return (
-    <article className="TodoInfo TodoInfo--completed">
+    <article
+      key={id}
+      className={`TodoInfo ${completed && 'TodoInfo--completed'}`}
+    >
       <h2 className="TodoInfo__title ">{title}</h2>
       {user && <UserInfo user={user} />}
     </article>
