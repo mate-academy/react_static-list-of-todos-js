2a86e993b1735a68d210d234d0d146a6117e8ab1
Yurii <uaremko945@gmail.com>
2024-07-28 10:26:58 +0300

    fixed tests

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -3,9 +3,11 @@ import { UserInfo } from '../UserInfo/UserInfo';
 import './TodoInfo.scss';
 
 export const TodoInfo = ({ todo }) => (
-  <article className={`TodoInfo ${todo.complited && 'TodoInfo--completed'}`}>
+  <article
+    className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+  >
     <h2 className="TodoInfo__title">{todo.title}</h2>
 
-    <UserInfo user={todo.user} key={todo.id} />
+    <UserInfo user={todo.user} />
   </article>
 );
