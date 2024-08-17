b70f57f30cb665bd127713693a09e9b03c17d07e
luter <newgglutt@gmail.com>
2024-08-17 17:28:31 +0300

    news

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo }) => (
-  <article className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}>
+  <article
+    className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+  >
     <h2 className="TodoInfo__title">{todo.title}</h2>
     <UserInfo user={todo.user} />
   </article>
