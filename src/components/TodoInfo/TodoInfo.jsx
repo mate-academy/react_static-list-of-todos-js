f0dd22d886ff78f63c906686b0c746cf06d9509e
Roma Zuravliov <janmorozov1988@gmail.com>
2024-09-03 17:52:25 +0300

    add task solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,16 @@
 // Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+
+export const TodoInfo = ({ todo }) => {
+  const completedClass = todo.completed ? 'TodoInfo--completed' : '';
+
+  return (
+    <div className={`TodoInfo ${completedClass}`}>
+      <h3 className="TodoInfo__title">{todo.title}</h3>
+      {todo.user && (
+        <a className="UserInfo" href={`mailto:${todo.user.email}`}>
+          {todo.user.name}
+        </a>
+      )}
+    </div>
+  );
+};
