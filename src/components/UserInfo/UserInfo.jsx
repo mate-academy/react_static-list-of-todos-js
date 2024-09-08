a80e22a4009fafa24fb6907e96ac7daf0190efe0
hardaira <hardaira@yahoo.com>
2024-09-08 14:27:24 +0200

    add solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,6 +1,6 @@
 // Add the required props
-export const UserInfo = ({ user }) => (
-  <a className="UserInfo" href={'mailto:' + user.email}>
-    {user.name}
+export const UserInfo = ({ user: {name, email} }) => (
+  <a className="UserInfo" href={`mailto:${email}`}>
+    {name}
   </a>
 );
