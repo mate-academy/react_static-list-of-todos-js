24a09ee698db7abea4623b92efa6745e3d316c83
Bohdan Horiunov <b.horiunov@gmail.com>
2024-09-12 21:03:23 +0300

    Done

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,2 +1,8 @@
 // Add the required props
-export const UserInfo = () => <>UserInfo markup</>;
+// import users from 'C:\Users\gorun\tasks\projects\react_static-list-of-todos-js\src\api\users.json'
+
+export const UserInfo = ({ user }) => (
+  <a className="UserInfo" href={`mailto:${user.email}`}>
+    {user.name}
+  </a>
+);
