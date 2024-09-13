c222c10153eee89d37fb73d4b27e36fdbd19d210
Dmytro Ivasenko <dmtivasenko@gmail.com>
2024-09-13 17:44:43 +0700

    add task solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,5 +1,5 @@
 export const UserInfo = ({ user }) => (
-  <a className="UserInfo" href={`mailto: ${user.email}`}>
+  <a className="UserInfo" href={`mailto:${user.email}`}>
     {user.name}
   </a>
 );
