80a5fe03b41a8f1580fc85ce343bcb9c199aef5e
Sholudyvyy <stashohulia@gmail.com>
2024-09-18 09:03:40 +0300

    Solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,14 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import cn from 'classnames';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+export const TodoInfo = ({ todo }) => {
+  const { title, completed, user } = todo;
+
+  return (
+    <article className={cn('TodoInfo', { 'TodoInfo--completed': completed })}>
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      {user !== undefined && <UserInfo user={user} />}
+    </article>
+  );
+};
