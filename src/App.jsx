65aec5bdf8a9131498f204bf927feb6c374cf8be
Maksym Fedorchuk <maxeffect.apollo@gmail.com>
2024-09-20 17:47:52 +0300

    second  changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { TodoList } from './components/TodoList';
 export const App = () => (
   <div className="App">
     <h1 className="App__title">Static list of todos</h1>
+
     <TodoList todos={todosFromServer} users={usersFromServer} />
   </div>
 );
--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -2,14 +2,14 @@ import './TodoInfo.scss';
 
 import { UserInfo } from '../UserInfo';
 
-export const TodoInfo = ({ todo }) => {
-  const { title, user } = todo;
+export const TodoInfo = ({ todo, users }) => {
+  const { title, userId } = todo;
 
   return (
     <article className="TodoInfo TodoInfo--completed">
       <h2 className="TodoInfo__title">{title}</h2>
 
-      <UserInfo user={user} />
+      <UserInfo user={users.find(user => user.id === userId)} />
     </article>
   );
 };
