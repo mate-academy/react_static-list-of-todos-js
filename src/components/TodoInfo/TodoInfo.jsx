e35f234bae82027a9ffff0bfed41e66a46a1de7e
Maksym Fedorchuk <maxeffect.apollo@gmail.com>
2024-09-20 17:51:04 +0300

    falsy todos

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -3,13 +3,13 @@ import './TodoInfo.scss';
 import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo, users }) => {
-  const { title, userId } = todo;
+  const { title, userId, completed } = todo;
 
   return (
-    <article className="TodoInfo TodoInfo--completed">
+    <article className={`TodoInfo ${completed ? 'TodoInfo--completed' : ''}`}>
       <h2 className="TodoInfo__title">{title}</h2>
 
-      <UserInfo user={users.find(user => user.id === userId)} />
+      <UserInfo user={users.find((user) => user.id === userId)} />
     </article>
   );
 };
