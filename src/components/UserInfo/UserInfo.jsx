8ff1c6701f154852ccc6b65b2491b40eb9206012
Igor Novikov <novikov.igor.20@gmail.com>
2024-09-20 18:56:46 +0300

    react_static-list-of-todos-js

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,10 +1,8 @@
-import './UserInfo';
-
 export const UserInfo = ({ user }) => {
   const { email, name } = user;
 
   return (
-    <a className="UserInfo" href="{`mailto:${email}`}">
+    <a className="UserInfo" href={`mailto:${email}`}>
       {name}
     </a>
   );
