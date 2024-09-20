9b14eeaac866146d36c611b2806f4cd2ff9e9ad6
Maksym Fedorchuk <maxeffect.apollo@gmail.com>
2024-09-20 17:56:53 +0300

    find const

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -4,12 +4,13 @@ import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo, users }) => {
   const { title, userId, completed } = todo;
+  const findUser = id => users.find(user => user.id === id);
 
   return (
     <article className={`TodoInfo ${completed ? 'TodoInfo--completed' : ''}`}>
       <h2 className="TodoInfo__title">{title}</h2>
 
-      <UserInfo user={users.find(user => user.id === userId)} />
+      <UserInfo user={findUser(userId)} />
     </article>
   );
 };
