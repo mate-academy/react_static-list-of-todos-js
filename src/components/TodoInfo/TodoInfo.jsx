20d41e7034424705ec216968b63cb35017115eaa
Vladimir Orlov <orelik392@gmail.com>
2024-10-02 02:14:41 +0300

    completed 10

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,26 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import './TodoInfo.scss';
+
+import { UserInfo } from '../UserInfo/UserInfo';
+
+export const TodoInfo = ({ todo }) => {
+  const todoClass = todo.completed
+    ? 'TodoInfo TodoInfo--completed'
+    : 'TodoInfo';
+  const { title, user } = todo;
+
+  return (
+    <article className={todoClass}>
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      <UserInfo user={user} />
+    </article>
+  );
+};
+
+/*
+      <article className="TodoInfo TodoInfo--completed">
+        <h2 className="TodoInfo__title">HTML</h2>
+
+      </article>
+
+*/
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,2 +1,18 @@
-// Add the required props
-export const UserInfo = () => <>UserInfo markup</>;
+import './UserInfo.scss';
+
+export const UserInfo = ({ user }) => {
+  const { email, name } = user;
+
+  return (
+    <a href={`mailto:${email}`} className="UserInfo">
+      {name}
+    </a>
+  );
+};
+
+/*
+        <a className="UserInfo" href="mailto:Sincere@april.biz">
+          Leanne Graham
+        </a>
+
+*/
