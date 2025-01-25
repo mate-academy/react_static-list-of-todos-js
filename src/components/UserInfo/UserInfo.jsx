1bb1a86bef3d9274ed087c05bd667c95e2f4494a
Valentyna <adm@adms-MacBook-Pro.local>
2025-01-25 22:13:50 +0200

    add react static list of todos js

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,2 +1,12 @@
 // Add the required props
-export const UserInfo = () => <>UserInfo markup</>;
+
+import React from 'react';
+import './UserInfo.scss';
+
+export const UserInfo = ({ user }) => {
+  return (
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      <span className="UserInfo__name">{user.name}</span>
+    </a>
+  );
+};
