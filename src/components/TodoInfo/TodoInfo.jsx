e3a43a9e559f60736a3db1a2552796096983fcfe
Ruslan <ruslan.konoplya@gmail.com>
2025-01-28 17:34:25 +0200

    add sol

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,15 @@
 // Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import { UserInfo } from '../UserInfo/UserInfo';
+
+export const TodoInfo = ({ todo }) => (
+  <article
+    className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+    key={todo.id}
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+
+    {todo.userId && <UserInfo user={todo.user} />}
+  </article>
+);
+
+export default TodoInfo;
