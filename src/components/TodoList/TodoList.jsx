3fecd06d3bfcee42b48cfc695799e11c38574ba3
Aznhelika Khamaza <anzhelikakhamaza@gmail.com>
2025-01-31 13:44:50 -0600

    Initial Commit

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,10 @@
-// Add the required props
-export const TodoList = () => <>TodoList markup</>;
+// eslint-disable-next-line import/named
+import { TodoInfo } from '../TodoInfo';
+
+export const TodoList = ({ todos }) => (
+  <div className="toDosInfo">
+    {todos.map(todo => (
+      <TodoInfo todo={todo} key={todo.id} />
+    ))}
+  </div>
+);
