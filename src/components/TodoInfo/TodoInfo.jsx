4dbb43e745134c12d911ff9c485ad2f37abc7124
Ovramenko Andrii <anovramenko2@gmail.com>
2025-03-02 18:55:40 +0200

    add task solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,9 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import { UserInfo } from '../UserInfo/UserInfo';
+import './TodoInfo.scss';
+
+export const TodoInfo = ({ todo: { userId, title, user, completed } }) => (
+  <article className={`TodoInfo ${completed && 'TodoInfo--completed'}`}>
+    <h2 className="TodoInfo__title">{title}</h2>
+    {user && <UserInfo user={user} key={userId} />}
+  </article>
+);
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,2 +1,7 @@
-// Add the required props
-export const UserInfo = () => <>UserInfo markup</>;
+import './UserInfo.scss';
+
+export const UserInfo = ({ user: { email, name } }) => (
+  <a className="UserInfo" href={`mailto:${email}`}>
+    {name}
+  </a>
+);
