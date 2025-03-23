96227de07e5a31cd7101d57b0027495e44fa636c
Andriian <an.hrytsan@gmail.com>
2025-03-23 15:55:52 +0200

    added comment for AI Buddy

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -3,6 +3,7 @@ import { TodoInfo } from '../TodoInfo/TodoInfo';
 export const TodoList = ({ todos }) => (
   <section className="TodoList">
     {todos.map(todo => (
+      // There is a key prop in the TodoInfo component within your TodoList component.
       <TodoInfo todo={todo} key={todo.id} />
     ))}
   </section>
