149761abeb06f9ddebae9407a837a471bdc2ce39
Andriian <an.hrytsan@gmail.com>
2025-03-23 15:52:07 +0200

    Solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,14 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+// eslint-disable-next-line import/no-extraneous-dependencies
+import clsx from 'clsx';
+
+import { UserInfo } from '../UserInfo/UserInfo';
+
+export const TodoInfo = ({ todo }) => (
+  <article
+    className={clsx('TodoInfo', { 'TodoInfo--completed': todo.completed })}
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+
+    {todo.user && <UserInfo user={todo.user} />}
+  </article>
+);
