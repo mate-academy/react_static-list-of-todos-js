887cb71b6a1f7976348a65da2f8e0a19bbe22060
Olexandr Ivaschenko <sasaivasenko9@gmail.com>
2025-05-14 15:39:57 +0300

    create UserInfo, TodoList, TodoInfo components

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,10 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import { UserInfo } from '../UserInfo';
+import './TodoInfo.scss';
+
+export const TodoInfo = ({ title, completed, user }) => (
+  <article className={`TodoInfo ${completed ? 'TodoInfo--completed' : ''}`}>
+    <h2 className="TodoInfo__title">{title}</h2>
+
+    <UserInfo user={user} />
+  </article>
+);
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,14 @@
-// Add the required props
-export const TodoList = () => <>TodoList markup</>;
+import { TodoInfo } from '../TodoInfo';
+
+export const TodoList = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map(todo => (
+      <TodoInfo
+        key={todo.id}
+        title={todo.title}
+        completed={todo.completed}
+        user={todo.user}
+      />
+    ))}
+  </section>
+);
