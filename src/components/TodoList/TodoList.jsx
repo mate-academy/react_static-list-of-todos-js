5ddaa615c03fc18080f3f6ab559b7c6062c408fb
hatevadym <hatevadym@gmail.com>
2025-05-26 13:29:25 +0200

    Update2

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,12 +1,33 @@
-// Add the required props
-// export const TodoList = () => <>TodoList markup</>;
-
-import { TodoInfo } from '../TodoInfo';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 export const TodoList = ({ todos }) => (
   <section className="TodoList">
     {todos.map(todo => (
-      <TodoInfo key={todo.id} todo={todo} />
+      <article
+        key={todo.id}
+        className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+      >
+        <h2 className="TodoInfo__title">{todo.title}</h2>
+
+        <a className="UserInfo" href={`mailto:${todo.user.email}`}>
+          {todo.user.name}
+        </a>
+      </article>
     ))}
   </section>
 );
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      completed: PropTypes.bool.isRequired,
+      user: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        email: PropTypes.string.isRequired,
+      }).isRequired,
+    }),
+  ).isRequired,
+};
