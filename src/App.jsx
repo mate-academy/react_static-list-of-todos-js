e55d0573f53b2df1c4e3563c37733ee7f7f060ae
Maryna <doctor.who2016.dw@gmail.com>
2025-05-29 18:37:37 +0300

    react static list of todos

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function getUserById(userId) {
   return usersFromServer.find(user => user.id === userId) || null;
 }
 
-export const todos = todosFromServer.map(todo => ({
+const todos = todosFromServer.map(todo => ({
   ...todo,
   user: getUserById(todo.userId),
 }));
