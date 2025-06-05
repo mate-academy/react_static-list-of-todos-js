00b90d3e56d453e00246ad9111dfc0fc4d60c6c7
petrushinsky-kirill <petrushinsky@gmail.com>
2025-06-05 22:08:45 +0300

    solution upd

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -2,7 +2,9 @@ import './TodoInfo.scss';
 import { UserInfo } from '../UserInfo/UserInfo';
 
 export const TodoInfo = ({ todo }) => (
-  <article className={`TodoInfo ${todo.completed && 'TodoInfo--completed'}`}>
+  <article
+    className={`${todo.completed ? 'TodoInfo TodoInfo--completed' : 'TodoInfo'}`}
+  >
     <h2 className="TodoInfo__title">{todo.title}</h2>
 
     <UserInfo user={todo.user} />
