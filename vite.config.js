8bc73fcefbdc5528072fe804db9523984f5b3fb3
IVANIVALDO <142198929+IVANIVALDO@users.noreply.github.com>
2025-07-22 09:47:22 -0300

    Finaliza tarefa com separação de componentes e estilo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react_static-list-of-todos/', // nome do repositório no GitHub
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+  },
+});
