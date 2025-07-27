77ab5ff4837ab422d7b89a5c25a2702c7a9331ec
michelam-pseudo <124099775+michelam-pseudo@users.noreply.github.com>
2025-07-27 03:13:22 -0300

    Decomposição orientada a componentes sob os requisitos especificados

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,2 +1,15 @@
 // Add the required props
-export const UserInfo = () => <>UserInfo markup</>;
+export const UserInfo = ({user}) => {
+    return(
+        <>
+
+            <a 
+                href={`mailto:${user.email}`} 
+                className="UserInfo"
+            >
+                {user.name}
+            </a>
+        
+        </>
+    )
+}
