4c694084f79e638d8920c2095646b988891bfd76
Poltorak Denys <itwfrontend@gmail.com>
2025-07-29 10:18:41 +0300

    add components to render users todo

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,11 @@
-// Add the required props
-export const TodoList = () => <>TodoList markup</>;
+import { TodoInfo } from '../TodoInfo';
+
+const TodoList = ({ todos }) => {
+  return todos.map(todo => (
+    <section key={todo.id} className="TodoList">
+      <TodoInfo todo={todo} />
+    </section>
+  ));
+};
+
+export { TodoList };
