8912aecf37f9405c4dd80d86bb5678620a25e45d
alina <kostenkoalina97@icloud.com>
2025-07-31 11:09:14 +0200

    remove uderId

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -4,7 +4,6 @@ export const TodoInfo = ({ todo }) => (
   <article
     className={`TodoInfo TodoInfo${todo.completed ? `--completed` : ``}`}
   >
-    {todo.userId}
     <h2 className="TodoInfo__title">{todo.title}</h2>
     {todo.user && <UserInfo user={todo.user} />}
   </article>
