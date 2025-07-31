c1de4367092629ea7dd861ef2b5a1f6f7463eb83
lvlashnin <lvlashnin@gmail.com>
2025-07-31 20:15:18 +0300

    realezed App through TodoList, TodoInfo, UserInfo components

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,8 @@
-// Add the required props
-export const TodoList = () => <>TodoList markup</>;
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
+export const TodoList = ({ todos }) =>
+  todos.map(todo => (
+    <section className="TodoList">
+      <TodoInfo todo={todo} key={todo.id} />
+    </section>
+  ));
