d173cea8785e4ac42f873f6793eb136f193194d0
Alessandro <Alessandrogabriel15160@gmail.com>
2025-07-31 10:40:39 -0300

    add solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,2 +1,11 @@
-// Add the required props
-export const UserInfo = () => <>UserInfo markup</>;
+export const UserInfo = ({ users }) => {
+  return (
+    <div className="UserInfo">
+      {users.map(user => (
+        <div key={user.id} className="UserInfo__item">
+          <h2 className="UserInfo__name">{user.name}</h2>
+        </div>
+      ))}
+    </div>
+  );
+};
