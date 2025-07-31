c395d76d472856fc8ae32ae309ce240880198c8c
Alessandro <Alessandrogabriel15160@gmail.com>
2025-07-31 10:52:17 -0300

    add solution1

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,6 @@ export const App = () => (
   <div className="App">
     <h1 className="App__title">Static list of todos</h1>
     <TodoList todos={todos} />
-    <UserInfo users={usersFromServer} />
+    <UserInfo user={todos[0].user} />
   </div>
 );
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,11 +1,8 @@
-export const UserInfo = ({ users }) => {
+export const UserInfo = ({ user }) => {
   return (
     <div className="UserInfo">
-      {users.map(user => (
-        <div key={user.id} className="UserInfo__item">
-          <h2 className="UserInfo__name">{user.name}</h2>
-        </div>
-      ))}
+      <h2 className="UserInfo__name">{user.name}</h2>
+      <p>{user.email}</p>
     </div>
   );
 };
