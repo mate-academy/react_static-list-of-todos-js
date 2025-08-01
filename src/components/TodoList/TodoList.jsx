749ada0a2be456ef2ae9723fc61a774d838e2e16
nesms <serhiy.nesmeyanov@qpard.com>
2025-08-01 11:57:54 +0300

    fix: key for map

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -5,7 +5,7 @@ export const TodoList = ({ todos }) => {
   return (
     <section className="TodoList">
       {todos.map(todo => (
-        <TodoInfo todo={todo} />
+        <TodoInfo todo={todo} key={todo.id} />
       ))}
     </section>
   );
