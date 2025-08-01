1cd00daade7701a035a37be7d3dae93509e21603
nesms <serhiy.nesmeyanov@qpard.com>
2025-08-01 12:00:55 +0300

    fix: classname

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,10 +1,13 @@
 // Add the required props
 import { UserInfo } from '../UserInfo';
+import classNames from 'classnames';
 
 export const TodoInfo = ({ todo }) => {
   return (
     <article
-      className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : null}`}
+      className={classNames('TodoInfo', {
+        'TodoInfo--completed': todo.completed,
+      })}
     >
       <h2 className="TodoInfo__title">{todo.title}</h2>
 
