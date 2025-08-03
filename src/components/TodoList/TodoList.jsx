05923e1726c355bbaf70f6ee334eb4f30d596ca0
Aweiro <arsenskott@gmail.com>
2025-08-03 15:53:23 +0300

    add task solution 1

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -4,7 +4,7 @@ import { TodoInfo } from '../TodoInfo';
 export const TodoList = ({ todos }) => (
   <section className="TodoList">
     {todos.map(todo => (
-      <TodoInfo todo={todo} user={todo.user} key={todo.id} />
+      <TodoInfo todo={todo} key={todo.id} />
     ))}
   </section>
 );
