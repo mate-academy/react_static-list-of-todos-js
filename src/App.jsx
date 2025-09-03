81eb0146e620505f6a9707cba2615226e3804e05
Denys Herych <fojjof2@gmail.com>
2025-09-03 14:02:03 +0200

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,6 @@ export const todos = todosFromServer.map(todo => ({
 export const App = () => (
   <div className="App">
     <h1 className="App__title">Static list of todos</h1>
-    <TodoList user={usersFromServer} todos={todos} />
+    <TodoList todos={todos} />
   </div>
 );
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,7 +1,6 @@
 // Add the required props
-export const UserInfo = ({ user }) =>
-  user && (
-    <a className="UserInfo" href={`mailto:${user.email}`}>
-      {user.name}
-    </a>
-  );
+export const UserInfo = ({ user }) => (
+  <a className="UserInfo" href={`mailto:${user.email}`}>
+    {user.name}
+  </a>
+);
