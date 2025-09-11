c5f69c705cf1d1fa157cfa61f33139f28f974c98
Zaur <zaurmamedov339933@gmail.com>
2025-09-11 23:59:59 +0300

    Fixed

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -6,6 +6,6 @@ export const TodoInfo = ({ todo }) => (
     className={todo.completed ? 'TodoInfo TodoInfo--completed' : 'TodoInfo'}
   >
     <h2 className="TodoInfo__title">{todo.title}</h2>
-    <UserInfo user={todo?.user} />
+    {todo.user && <UserInfo user={todo.user} />}
   </article>
 );
