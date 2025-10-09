e7518ebbb768a9eb798549e3a027bc8860efae12
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-10-09 20:55:43 +0100

    Solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,9 @@
+import { UserInfo } from '../UserInfo/UserInfo';
+
 // Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+export const TodoInfo = ({ todo }) => (
+  <div className={`TodoInfo${todo.completed ? ' TodoInfo--completed' : ''}`}>
+    <p className="TodoInfo__title">{todo.title}</p>
+    <UserInfo user={todo.user} />
+  </div>
+);
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,13 @@
+import React from 'react';
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
 // Add the required props
-export const TodoList = () => <>TodoList markup</>;
+export const TodoList = ({ todos }) => (
+  <ul className="TodoList">
+    {todos.map(todo => (
+      <li key={todo.id} className="TodoList__item">
+        <TodoInfo todo={todo} />
+      </li>
+    ))}
+  </ul>
+);
