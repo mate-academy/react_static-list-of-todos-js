3392a6291febd31f5a1dddf3857e78e787ea2739
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-10-11 19:39:56 +0100

    Solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -4,6 +4,6 @@ import { UserInfo } from '../UserInfo/UserInfo';
 export const TodoInfo = ({ todo }) => (
   <div className={`TodoInfo${todo.completed ? ' TodoInfo--completed' : ''}`}>
     <p className="TodoInfo__title">{todo.title}</p>
-    <UserInfo user={todo.user} />
+    {todo.user && <UserInfo user={todo.user} />}
   </div>
 );
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -4,7 +4,5 @@ import React from 'react';
 export const UserInfo = ({ user }) => (
   <a className="UserInfo" href={`mailto:${user.email}`}>
     {user.name}
-    {user.email}
-    {user.phone}
   </a>
 );
