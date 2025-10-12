18654b4d58f09f7738074361dfce718cbc2aeb53
Afengi <gorodny.ivan4@gmail.com>
2025-10-12 14:39:42 +0300

    ашч_щту

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,5 +1,5 @@
 // Add the required props
-import { UserInfo } from '../UserInfo';
+import { UserInfo } from '../UserInfo/UserInfo';
 
 export const TodoInfo = ({ todo }) => {
   const articleClass = `TodoInfo${todo.completed ? ' TodoInfo--completed' : ''}`;
