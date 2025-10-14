23097dfe3ff0a35180c6193fc6638bf5240730bd
Vladyslav Kostiuk <K4nezoor@gmail.com>
2025-10-14 10:05:19 +0200

    finished

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,16 @@
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+import { UserInfo } from '../UserInfo/UserInfo';
+
 // Add the required props
-export const TodoList = () => <>TodoList markup</>;
+export const TodoList = ({ todos }) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo key={todo.id} todo={todo}>
+          <h2 className="TodoInfo__title">{todo.title}</h2>
+          {todo.user && <UserInfo user={todo.user} />}
+        </TodoInfo>
+      ))}
+    </section>
+  );
+};
