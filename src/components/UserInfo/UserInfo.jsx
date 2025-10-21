e4d51e7e29802859e040ff049ded7ba271244b70
Roma Ivashchenko <rrrrgvvggh@gmail.com>
2025-10-21 13:02:10 +0300

    add

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,5 +1,3 @@
 export const UserInfo = ({ user }) => (
-  <a className="UserInfo" href={`mailto:${user.email}`}>
-    {user.name}
-  </a>
+  <a className="UserInfo" href={`mailto:${user.email}`}>{user.name}</a>
 );
