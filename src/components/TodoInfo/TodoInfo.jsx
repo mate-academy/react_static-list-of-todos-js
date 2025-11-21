6ea98330e409707f54cca3d2d53fa09e3e0ecb6d
Your Name <u513509754@gmail.com>
2025-11-21 12:00:04 -0800

    fix

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -8,7 +8,7 @@ export const TodoInfo = ({ todo }) => {
   return (
     <article className={isComplete}>
       <h2 className="TodoInfo__title">{todo.title}</h2>
-      <UserInfo user={todo.user} />
+      {todo.user ? <UserInfo user={todo.user} /> : null}
     </article>
   );
 };
