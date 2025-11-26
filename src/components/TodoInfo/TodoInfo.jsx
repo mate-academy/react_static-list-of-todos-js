78a47bddaf6b9a52b55274ae7652e15c830def47
Sertavr <pampuh.net@gmail.com>
2025-11-26 21:19:45 +0200

    add task solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -5,12 +5,8 @@ export const TodoInfo = ({ todo }) => {
   const { title, completed, user } = todo;
 
   return (
-    <article className={`TodoInfo ${completed === true ? 'TodoInfo--completed' : ''}`}>
-      <h2
-        className="TodoInfo__title"
-      >
-        {title}
-      </h2>
+    <article className={`TodoInfo ${completed ? 'TodoInfo--completed' : ''}`}>
+      <h2 className="TodoInfo__title">{title}</h2>
       {user && <UserInfo user={user} />}
     </article>
   );
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,8 +1,12 @@
 import './UserInfo.scss';
 
-export const UserInfo = ({user}) => {
+export const UserInfo = ({ user }) => {
+  if (!user) {
+    return null;
+  }
+
   const { name, email } = user;
-  
+
   return (
     <a className="UserInfo" href={`mailto:${email}`}>
       {name}
