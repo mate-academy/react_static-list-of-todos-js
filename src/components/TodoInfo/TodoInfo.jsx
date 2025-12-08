f43d4b3e895e38e5ce55b5582bd30a4f864c42d3
Dmytro Nykorovych <dev.dmytronykorovych@gmail.com>
2025-12-08 21:38:40 +0100

    Solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,5 +1,5 @@
-import { UserInfo } from '../UserInfo/UserInfo';
 import cn from 'classnames';
+import { UserInfo } from '../UserInfo/UserInfo';
 
 export const TodoInfo = ({ todo }) => (
   <article
