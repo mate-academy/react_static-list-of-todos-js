4b6f904007619f97482690a5c8deb02183ed981b
Anton <nimachuka@gmail.com>
2025-12-26 17:20:06 +0100

    all tests passed

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,2 +1,9 @@
 // Add the required props
-export const UserInfo = () => <>UserInfo markup</>;
+// STEP_01
+export const UserInfo = ({ user }) => {
+  return (
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
+  );
+};
