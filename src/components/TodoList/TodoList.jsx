9769e3886c7889d0827df0d06f66285ba5c421d1
Alex <1inboxna@gmail.com>
2025-12-31 14:05:56 +0200

    commit message-1

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,43 +2,9 @@ import { TodoInfo } from '../TodoInfo/TodoInfo';
 
 // Add the required props
 export const TodoList = ({ todos }) => (
-  <>
+  <section className="TodoList">
     {todos.map(todo => {
       return <TodoInfo todo={todo} key={todo.id} />;
     })}
-  </>
+  </section>
 );
-
-// <section className="TodoList">
-//   <article className="TodoInfo TodoInfo--completed">
-//     <h2 className="TodoInfo__title">HTML</h2>
-
-//     <a className="UserInfo" href="mailto:Sincere@april.biz">
-//       Leanne Graham
-//     </a>
-//   </article>
-
-//   <article className="TodoInfo TodoInfo--completed">
-//     <h2 className="TodoInfo__title">CSS</h2>
-
-//     <a className="UserInfo" href="mailto:Sincere@april.biz">
-//       Leanne Graham
-//     </a>
-//   </article>
-
-//   <article className="TodoInfo TodoInfo--completed">
-//     <h2 className="TodoInfo__title">JS</h2>
-
-//     <a className="UserInfo" href="mailto:Shanna@melissa.tv">
-//       Ervin Howell
-//     </a>
-//   </article>
-
-//   <article className="TodoInfo">
-//     <h2 className="TodoInfo__title">React</h2>
-
-//     <a className="UserInfo" href="mailto:Nathan@yesenia.net">
-//       Clementine Bauch
-//     </a>
-//   </article>
-// </section>;
