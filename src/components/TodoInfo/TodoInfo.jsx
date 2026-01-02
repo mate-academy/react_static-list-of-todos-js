69c1e9274f419b97053766ab41390c7c4819aeeb
Nazar Kundys <nazarrisd@gmail.com>
2026-01-02 16:34:38 +0200

    ✨ feat(Todo): enhance TodoInfo and TodoList components   - Add `TodoInfo` component to display individual todo details   - Implement `UserInfo` component for user email linking   - Style `TodoInfo` and `UserInfo` with SCSS   - Update `TodoList` to render a list of `TodoInfo` components

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,14 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import { UserInfo } from '../UserInfo/UserInfo';
+import './TodoInfo.scss';
+
+export const TodoInfo = ({ todo: { title, completed, user } }) => {
+  return (
+    <article
+      className={`TodoInfo TodoInfo${(completed && '--completed') || ''}`}
+    >
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
