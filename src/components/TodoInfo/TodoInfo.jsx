99f655572932e752806e331360b7e5161c0dcc9e
Natalia Yakovleva <yakovlevane29@gmail.com>
2026-01-05 11:26:18 +0200

    fixed key

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -5,7 +5,6 @@ import { UserInfo } from '../UserInfo/UserInfo';
 
 export const TodoInfo = ({ todo }) => (
   <article
-    key={todo.id}
     className={todo.completed ? 'TodoInfo TodoInfo--completed' : 'TodoInfo'}
   >
     <h2 className="TodoInfo__title">{todo.title}</h2>
