2c335689734b1ad8c89f1dad494105c961810332
Natalia Yakovleva <yakovlevane29@gmail.com>
2026-01-05 11:16:14 +0200

    fixed solution

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ export const todos = todosFromServer.map(todo => ({
 export const App = () => (
   <div className="App">
     <h1 className="App__title">Static list of todos</h1>
-    <TodoList todos={todos} key={todos.id} />
+    <TodoList todos={todos} />
     {/* <section className="TodoList">
       <article className="TodoInfo TodoInfo--completed">
         <h2 className="TodoInfo__title">HTML</h2>
--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -5,6 +5,7 @@ import { UserInfo } from '../UserInfo/UserInfo';
 
 export const TodoInfo = ({ todo }) => (
   <article
+    key={todo.id}
     className={todo.completed ? 'TodoInfo TodoInfo--completed' : 'TodoInfo'}
   >
     <h2 className="TodoInfo__title">{todo.title}</h2>
