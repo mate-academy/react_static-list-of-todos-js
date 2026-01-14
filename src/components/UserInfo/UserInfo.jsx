34628e2e2035c2665f783372a417b4daae8986e5
unknown <valentina19960410@gmail.com>
2026-01-14 16:04:22 +0200

    add task solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,2 +1,6 @@
 // Add the required props
-export const UserInfo = () => <>UserInfo markup</>;
+export const UserInfo = ({ user }) => (
+  <a className="UserInfo" href="mailto:Nathan@yesenia.net">
+    {user.name}
+  </a>
+);
