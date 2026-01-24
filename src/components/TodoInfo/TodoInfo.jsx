43884e53e12c1b7ea95925ece84f32b7d54dff1f
Oleksandr Kozyriev <kozyrev.sania@gmail.com>
2026-01-24 03:12:03 +0200

    add solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,17 @@
+import clsx from 'clsx';
+import { UserInfo } from '../UserInfo';
+
 // Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+export const TodoInfo = ({ todo }) => {
+  return (
+    <article
+      className={clsx('TodoInfo', {
+        'TodoInfo--completed': todo.completed,
+      })}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      {todo.user && <UserInfo user={todo.user} />}
+    </article>
+  );
+};
