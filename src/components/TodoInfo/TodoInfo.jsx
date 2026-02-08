e117240aeca362f602373b0e19c736e2a4004441
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2026-02-08 15:10:19 +0200

    done React static list of TODOs (JS) task

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,17 @@
 // Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import React from 'react';
+import { UserInfo } from '../UserInfo/UserInfo';
+
+export const TodoInfo = ({ todo }) => {
+  const user = todo.user || { name: todo.author, email: todo.email };
+  const completedClass = todo.completed
+    ? 'TodoInfo TodoInfo--completed'
+    : 'TodoInfo';
+
+  return (
+    <div className={completedClass}>
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+      {todo.user && <UserInfo user={user} />}
+    </div>
+  );
+};
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,2 +1,16 @@
 // Add the required props
-export const UserInfo = () => <>UserInfo markup</>;
+import React from 'react';
+
+export const UserInfo = ({ user }) => {
+  const userInfoName = user.name;
+  const userInfoEmail = user.email;
+
+  return (
+    <div className="UserInfo">
+      <a className="UserInfo__name" href={`mailto:${userInfoEmail}`}>
+        {userInfoName}
+      </a>
+      <div className="UserInfo__email">{userInfoEmail}</div>
+    </div>
+  );
+};
