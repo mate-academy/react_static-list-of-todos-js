bc93e5d0a16708ef96570b312b5c0c168d343a7a
A-Melnykk <969696245n@gmail.com>
2026-02-10 18:06:42 +0200

    add task solution

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,20 @@
-// Add the required props
-export const TodoList = () => <>TodoList markup</>;
+import React from 'react';
+import { TodoInfo } from '../TodoInfo';
+
+export const TodoList = ({ todos }) => {
+  return (
+    <table className="table is-fullwidth is-striped" data-cy="TodoList">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>User</th>
+        </tr>
+      </thead>
+      <tbody>
+        {todos.map(todo => (
+          <TodoInfo key={todo.id} todo={todo} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,2 +1,9 @@
-// Add the required props
-export const UserInfo = () => <>UserInfo markup</>;
+import React from 'react';
+
+export const UserInfo = ({ user }) => {
+  return (
+    <a href={`mailto:${user.email}`} className="UserInfo" data-cy="UserInfo">
+      {user.name}
+    </a>
+  );
+};
