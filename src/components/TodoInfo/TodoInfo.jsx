609ab848977b531f611840fb1f84a9223cc12eab
Mariia <proninamaria2002@gmail.com>
2026-02-12 19:48:20 +0200

    Pronina_develop

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,8 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import React from 'react';
+import { UserInfo } from '../UserInfo/UserInfo';
+export const TodoInfo = ({ todo }) => (
+<div className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`} data-cy="TodoInfo" >
+<p className="TodoInfo__title">
+{todo.title}</p>
+{todo.user && <UserInfo user={todo.user} />}
+</div> );
