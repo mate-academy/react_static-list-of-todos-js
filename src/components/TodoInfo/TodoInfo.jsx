4209bee8a2e07d96f249ea9fc266d835ca9eb492
ikseloi <gdalexgo@gmail.com>
2026-04-14 11:02:44 +0300

    fix: address code review feedback

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,12 +1,14 @@
 import './TodoInfo.scss';
-import clsx from 'clsx';
 import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo }) => {
   const { title, completed, user } = todo;
+  const containerClass = completed
+    ? 'TodoInfo TodoInfo--completed'
+    : 'TodoInfo';
 
   return (
-    <article className={clsx('TodoInfo', { 'TodoInfo--completed': completed })}>
+    <article className={containerClass}>
       <h2 className="TodoInfo__title">{title}</h2>
 
       {user && <UserInfo user={user} />}
