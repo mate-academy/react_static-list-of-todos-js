2cb524cdaa8dcef7c0ea6447792c2ac7215e08eb
kennyprog <overbafer08@gmail.com>
2026-04-18 13:47:04 +0300

    Luke dostal menya

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,9 +1,11 @@
 // Add the required props
-import { UserInfo } from "../UserInfo/UserInfo";
+import { UserInfo } from '../UserInfo/UserInfo';
 
 export const TodoInfo = ({ todo }) => (
-  <article className={`${todo.completed ? "TodoInfo TodoInfo--completed" : "TodoInfo"}`}>
+  <article
+    className={`${todo.completed ? 'TodoInfo TodoInfo--completed' : 'TodoInfo'}`}
+  >
     <h2 className="TodoInfo__title">{todo.title}</h2>
-     <UserInfo user={todo.user}/>
+    {todo.user && <UserInfo user={todo.user} />}
   </article>
 );
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,8 +1,5 @@
-import { TodoInfo } from "../TodoInfo";
+import { TodoInfo } from '../TodoInfo/TodoInfo';
 
 // Add the required props
-export const TodoList = ({ todos }) => (
-  todos.map(todo => (
-    <TodoInfo todo={todo} key={todo.id}/>
-  ))
-);
+export const TodoList = ({ todos }) =>
+  todos.map(todo => <TodoInfo todo={todo} key={todo.id} />);
