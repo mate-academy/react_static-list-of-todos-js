c61deea987fd9ccc9704baf99055168d38cc9a29
OlehYuriev <urievoleg94@gmail.com>
2026-04-20 18:52:48 +0300

    fix UserInfo

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -4,10 +4,10 @@ import { UserInfo } from '../UserInfo/UserInfo';
 export const TodoInfo = ({ todo }) => {
   return (
     <article
-      className={`TodoInfo  ${todo.completed ? 'TodoInfo--completed' : ''} `}
+      className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''} `}
     >
       <h2 className="TodoInfo__title">{todo.title}</h2>
-      <UserInfo user={todo.user} />
+      {todo.user && <UserInfo user={todo.user} />}
     </article>
   );
 };
