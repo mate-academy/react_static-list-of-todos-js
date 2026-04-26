952d3bcecb81e3fc82fd37eb1a065e77cc80bb10
Maksim Vashuk <maximka.vashuk@gmail.com>
2026-04-26 20:29:27 +0200

    fix key{todo.id}

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -4,7 +4,6 @@ import { UserInfo } from '../UserInfo/UserInfo';
 export const TodoInfo = ({ todo }) => {
   return (
     <article
-      key={todo.id}
       className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
     >
       <h2 className="TodoInfo__title">{todo.title}</h2>
