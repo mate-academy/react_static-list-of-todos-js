4ccb109e989c166f09190d627a39a2a58b57a579
Andii Karamushko <karamushko1997@gmail.com>
2026-05-05 23:44:04 +0300

    solution

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,19 @@
 // Add the required props
-export const TodoList = () => <>TodoList markup</>;
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
+export const TodoList = ({ todos }) => {
+  return (
+    <section className="TodoList">
+      {/* <article className="TodoInfo TodoInfo--completed">
+        <h2 className="TodoInfo__title">HTML</h2>
+
+        <a className="UserInfo" href="mailto:Sincere@april.biz">
+          Leanne Graham
+        </a>
+      </article> */}
+      {todos.map(item => (
+        <TodoInfo todo={item} key={item.id} />
+      ))}
+    </section>
+  );
+};
