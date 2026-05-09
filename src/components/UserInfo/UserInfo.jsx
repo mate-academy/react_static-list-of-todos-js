0b1779316550e36a0fb07f79725d9b7abc7d092e
Natalia Lunkevych <gadyka08@gmail.com>
2026-05-09 16:25:59 +0300

    1

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,2 +1,8 @@
+import React from 'react';
+import '../../api/users';
 // Add the required props
-export const UserInfo = () => <>UserInfo markup</>;
+export const UserInfo = ({ user }) => (
+  <a className="UserInfo" href={`mailto:${user.email}`}>
+    {user.name}
+  </a>
+);
