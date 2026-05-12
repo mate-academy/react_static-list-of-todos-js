8ca4cae178170bc0900b531759a9b5db7a68eb44
Volodymyr Gorbovuy <vova.gorbovuy@gmail.com>
2026-05-13 01:47:03 +0200

    fix id null

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -4,8 +4,8 @@ export const TodoInfo = ({ todo }) => (
   <article
     className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
   >
-    <h2 className="TodoInfo__title">{todo.title}</h2>
+    <h2 className="TodoInfo__title">{`${todo.title}`}</h2>
 
-    <UserInfo user={todo.user} />
+    {todo.user && <UserInfo user={todo.user} />}
   </article>
 );
