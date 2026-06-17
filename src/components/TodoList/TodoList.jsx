35b3c4afb0c24ef695a1eee99e1e326ec0fd773f
Andrii Nemshylov <nemshylovandrii1290@gmail.com>
2026-06-17 20:06:59 +0200

    Fix review comments

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,14 +1,10 @@
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
 // Add the required props
 export const TodoList = ({ todos }) => (
   <section className="TodoList">
     {todos.map(todo => (
-      <article key={todo.id} className="TodoInfo TodoInfo--completed">
-        <h2 className="TodoInfo__title">{todo.title}</h2>
-
-        <a className="UserInfo" href={`mailto:${todo.user.email}`}>
-          {todo.user.name}
-        </a>
-      </article>
+      <TodoInfo key={todo.id} todo={todo} />
     ))}
   </section>
 );
