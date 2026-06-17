f609a79246c8fb177634a35e93f648978e3d97ef
Andrii Nemshylov <nemshylovandrii1290@gmail.com>
2026-06-17 19:45:57 +0200

    Split todo list into components

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,14 @@
 // Add the required props
-export const TodoList = () => <>TodoList markup</>;
+export const TodoList = ({ todos }) => (
+  <section className="TodoList">
+    {todos.map(todo => (
+      <article key={todo.id} className="TodoInfo TodoInfo--completed">
+        <h2 className="TodoInfo__title">{todo.title}</h2>
+
+        <a className="UserInfo" href={`mailto:${todo.user.email}`}>
+          {todo.user.name}
+        </a>
+      </article>
+    ))}
+  </section>
+);
