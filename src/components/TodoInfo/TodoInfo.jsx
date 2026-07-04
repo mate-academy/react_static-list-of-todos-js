837772933226a6f8e596dab9ed6ebfd553344095
Anik <anikferreira8@gmail.com>
2026-07-03 23:06:19 -0300

    Complete React Static List of TODOs

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,18 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import { UserInfo } from '../UserInfo/UserInfo';
+
+export const TodoInfo = ({ todo }) => {
+  return (
+    <div
+      className={todo.completed ? 'TodoInfo TodoInfo--completed' : 'TodoInfo'}
+      data-cy="Todo"
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      <p className="TodoInfo__status">
+        {todo.completed ? 'Completed' : 'Active'}
+      </p>
+
+      {todo.user && <UserInfo user={todo.user} />}
+    </div>
+  );
+};
