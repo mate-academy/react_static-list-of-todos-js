345c49dd849771099d703deade3c88027f64618a
student1232354 <tarasromanenko33@gmail.com>
2026-07-13 12:39:58 +0300

    first update

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,15 @@
-// Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import { UserInfo } from '../UserInfo/UserInfo';
+
+export const TodoInfo = ({ todo, user }) => {
+  const currentUser = user || todo.user;
+
+  return (
+    <article
+      className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+    >
+      <h2 className="TodoInfo__title">{todo.title}</h2>
+
+      {currentUser && <UserInfo user={currentUser} />}
+    </article>
+  );
+};
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,12 @@
-// Add the required props
-export const TodoList = () => <>TodoList markup</>;
+import React from 'react';
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
+export const TodoList = ({ todos, users }) => (
+  <>
+    {todos.map(todo => {
+      const user = users?.find(u => u.id === todo.userId);
+
+      return <TodoInfo key={todo.id} todo={todo} user={user} />;
+    })}
+  </>
+);
