bb20e256958322d02d1fee95d035e22eb5869a3e
AndSLASH <andrey.zaharov.93@gmail.com>
2026-08-10 16:16:33 +0300

    Solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,2 +1,11 @@
-// Add the required props
-export const UserInfo = () => <>UserInfo markup</>;
+export const UserInfo = props => {
+  const {
+    user: { email, name },
+  } = props;
+
+  return (
+    <a className="UserInfo" href={`mailto:${email}`}>
+      {name}
+    </a>
+  );
+};
