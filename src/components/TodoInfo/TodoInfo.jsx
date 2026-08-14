cd4031f3cddf68fdfc3aae57d60a57dc8303e558
diiyavol <nikitanesterenko88@gmail.com>
2026-08-14 19:58:53 +0300

    solution 3

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -3,6 +3,7 @@ import { UserInfo } from '../UserInfo/UserInfo';
 export const TodoInfo = ({ todo }) => (
   <article
     className={todo.completed ? 'TodoInfo TodoInfo--completed' : 'TodoInfo'}
+    key={todo.id}
   >
     <h2 className="TodoInfo__title">{todo.title}</h2>
     {todo.user !== null ? <UserInfo user={todo.user} /> : undefined}
