644bd1f620f18b47967fdcfb119c0ee5eb916363
Oleksandr <oleksandr.sviderskyi@nure.ua>
2026-08-15 17:21:07 +0300

    add condition for user

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -4,6 +4,6 @@ export const TodoInfo = ({ todo }) => (
   <article className={`TodoInfo ${todo.completed && 'TodoInfo--completed'}`}>
     <h2 className="TodoInfo__title">{todo.title}</h2>
 
-    <UserInfo user={todo.user} />
+    {todo.user && <UserInfo user={todo.user} />}
   </article>
 );
