ab47472b1920815ac2837f6a5ba55508b0730366
Oleksandr <oleksandr.sviderskyi@nure.ua>
2026-08-15 17:14:36 +0300

    change user find logic

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,10 +1,9 @@
 import { UserInfo } from '../UserInfo';
-import usersFromServer from '../../api/users.json';
 
 export const TodoInfo = ({ todo }) => (
   <article className={`TodoInfo ${todo.completed && 'TodoInfo--completed'}`}>
     <h2 className="TodoInfo__title">{todo.title}</h2>
 
-    <UserInfo user={usersFromServer[todo.userId - 1]} />
+    <UserInfo user={todo.user} />
   </article>
 );
