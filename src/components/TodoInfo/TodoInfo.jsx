fc4104839ca7ccf18cfbeba3bf944a84dfc73418
Gregory Lyon <ggylyon@gmail.com>
2026-08-25 20:25:15 +0300

    Made the requested changes

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -11,7 +11,7 @@ export const TodoInfo = ({ todo }) => {
     >
       <h2 className="TodoInfo__title">{todo.title}</h2>
 
-      <UserInfo user={todo.user} />
+      {todo.user && <UserInfo user={todo.user} />}
     </article>
   );
 };
