d79e966d713bd58b65a7b20b63551dbd986ba835
Vladyslav <vlad.lozynsky@gmail.com>
2026-08-25 16:17:53 +0300

    Issue resolved

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -14,7 +14,7 @@ export const TodoInfo = ({ todo }) => {
     <article className={`TodoInfo ${getClassOfTodo(todo.completed)}`}>
       <h2 className="TodoInfo__title">{todo.title}</h2>
 
-      <UserInfo user={todo.user} />
+      {todo.user && <UserInfo user={todo.user} />}
     </article>
   );
 };
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,6 +1,10 @@
 import './UserInfo.scss';
 
 export const UserInfo = ({ user }) => {
+  if (!user) {
+    return null;
+  }
+
   return (
     <a className="UserInfo" href={`mailto:${user.email}`}>
       {user.name}
