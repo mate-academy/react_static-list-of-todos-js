41718307069469472cfb14ff3bbb61d0823affc8
Mykyta Arkatov <nikitaarkatov4@gmail.com>
2026-08-26 19:27:36 +0300

    Refactor TodoInfo className

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -2,7 +2,7 @@ import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo }) => (
   <article
-    className={todo.completed ? 'TodoInfo TodoInfo--completed' : 'TodoInfo'}
+    className={`TodoInfo${todo.completed ? ' TodoInfo--completed' : ''}`}
   >
     <h2 className="TodoInfo__title">{todo.title}</h2>
 
