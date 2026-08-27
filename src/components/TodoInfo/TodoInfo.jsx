40ce1d31c7cfdd3e2c5c8520a1179be7b53ac370
Artem Mayboroda <artmay77777@gmail.com>
2026-08-27 22:10:33 +0300

    Clarify TodoList key handling

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -2,9 +2,7 @@ import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo }) => (
   <article
-    className={
-      todo.completed ? 'TodoInfo TodoInfo--completed' : 'TodoInfo'
-    }
+    className={todo.completed ? 'TodoInfo TodoInfo--completed' : 'TodoInfo'}
   >
     <h2 className="TodoInfo__title">{todo.title}</h2>
 
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,6 +1,6 @@
 import { TodoInfo } from '../TodoInfo';
 
-export const TodoList = ({ todos }) => (
+export const TodoList = ({ todos = [] }) => (
   <section className="TodoList">
     {todos.map(todo => (
       <TodoInfo key={todo.id} todo={todo} />
