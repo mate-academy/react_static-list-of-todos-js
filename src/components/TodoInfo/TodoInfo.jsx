c5a35e6f35c995e4f8b66ee1d1dc15d99d7d27e6
Santana <wonderday1804@gmail.com>
2026-08-30 15:55:12 +0300

    improve todoInfo readability

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,12 +1,15 @@
 import { UserInfo } from '../UserInfo/UserInfo';
 
-export const TodoInfo = ({ todo }) => (
-  <article
-    className={
-      todo.completed === false ? 'TodoInfo' : 'TodoInfo TodoInfo--completed'
-    }
-  >
-    {todo.user && <UserInfo user={todo.user} />}
-    <h2 className="TodoInfo__title">{todo.title}</h2>
-  </article>
-);
+export const TodoInfo = ({ todo }) => {
+  const { title, completed, user } = todo;
+
+  const todoClassName = completed ? 'TodoInfo TodoInfo--completed' : 'TodoInfo';
+
+  return (
+    <article className={todoClassName}>
+      <h2 className="TodoInfo__title">{title}</h2>
+
+      {user && <UserInfo user={user} />}
+    </article>
+  );
+};
