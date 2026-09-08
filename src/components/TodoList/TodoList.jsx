3d455e4f25a8cb64d03a17bbf3f9fa883fb9b1aa
ALex <sasha.hivuk@gmail.com>
2026-09-08 15:51:59 +0300

    add key

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -4,7 +4,7 @@ import { TodoInfo } from '../TodoInfo/TodoInfo';
 export const TodoList = ({ todos }) => (
   <section className="TodoList">
     {todos.map(todo => (
-      <TodoInfo todo={todo} />
+      <TodoInfo todo={todo} key={todo.id} />
     ))}
   </section>
 );
