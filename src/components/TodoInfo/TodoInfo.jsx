63f5def72342d4ec4e864f47d423e896045df5c2
Sumar Illya <illasumar002@gmail.com>
2026-09-12 14:01:41 +0300

    User now render only if user is exist

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -5,6 +5,6 @@ export const TodoInfo = ({ todo: { title, completed, user } }) => (
   <article className={cn('TodoInfo', { 'TodoInfo--completed': completed })}>
     <h2 className="TodoInfo__title">{title}</h2>
 
-    <UserInfo user={user} />
+    {user && <UserInfo user={user} />}
   </article>
 );
